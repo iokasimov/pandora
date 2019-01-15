7bb3de581c4e2e46010310be47550d8f52a8b330
Murat Kasimov <iokasimov.m@gmail.com>
2019-01-15 17:21:43 +0300

    Define Morphism module

---
+++ Morphism.hs
@@ -0,0 +1,31 @@
+module Morphism (identity, flip, fix, (.), ($), (&), (!)) where
+
+infixr 9 .
+infixr 0 $
+infixl 1 &
+infixr 2 !
+
+{-# INLINE identity #-}
+identity :: a -> a
+identity x = x
+
+{-# INLINE flip #-}
+flip :: (a -> b -> c) -> b -> a -> c
+flip f x y = f y x
+
+fix :: (a -> a) -> a
+fix f = let x = f x in x
+
+{-# INLINE (.) #-}
+f . g = \x -> f (g x)
+
+{-# INLINE ($) #-}
+f $ x = f x
+
+{-# INLINE (&) #-}
+(&) :: a -> (a -> b) -> b
+x & f = f x
+
+{-# INLINE (!) #-}
+(!) :: a -> b -> a
+x ! y = x
