156ecf9744b6c167099dff9584904ff1974b118f
Murat Kasimov <iokasimov.m@gmail.com>
2019-01-16 18:53:10 +0300

    Add Conclusion and Product paradigms

---
+++ Paradigm/Conclusion.hs
@@ -0,0 +1,37 @@
+module Paradigm.Conclusion (Conclusion (..)) where
+
+import Core.Morphism ((.), ($))
+import Pattern.Functor.Covariant (Covariant ((<$>)))
+import Pattern.Functor.Pointable (Pointable (point))
+import Pattern.Functor.Alternative (Alternative ((<+>)))
+import Pattern.Functor.Applicative (Applicative ((<*>)))
+import Pattern.Functor.Traversable (Traversable ((->>)))
+import Pattern.Functor.Bindable (Bindable ((>>=)))
+import Pattern.Functor.Monad (Monad)
+
+data Conclusion e a = Failure e | Success a
+
+instance Covariant (Conclusion e) where
+	f <$> Success x = Success $ f x
+	_ <$> Failure y = Failure y
+
+instance Pointable (Conclusion e) where
+	point = Success
+
+instance Applicative (Conclusion e) where
+	Success f <*> x = f <$> x
+	Failure y <*> x = Failure y
+
+instance Alternative (Conclusion e) where
+	Failure y <+> x = x
+	Success x <+> y = Success x
+
+instance Traversable (Conclusion e) where
+	Failure y ->> _ = point $ Failure y
+	Success x ->> f = Success <$> f x
+
+instance Bindable (Conclusion e) where
+	Success x >>= f = f x
+	Failure y >>= _ = Failure y
+
+instance Monad (Conclusion e) where
---
+++ Paradigm/Product.hs
@@ -0,0 +1,24 @@
+module Paradigm.Product (Product (..), type (:&:)) where
+
+import Core.Morphism (($))
+import Pattern.Functor.Covariant (Covariant ((<$>)))
+import Pattern.Functor.Extractable (Extractable (extract))
+import Pattern.Functor.Extendable (Extendable ((=>>)))
+import Pattern.Functor.Comonad (Comonad)
+
+infixr 1 :&:
+
+data Product a b = a :&: b
+
+type (:&:) = Product
+
+instance Covariant (Product a) where
+	f <$> (x :&: y) = x :&: f y
+
+instance Extractable (Product a) where
+	extract (x :&: y) = y
+
+instance Extendable (Product a) where
+	(x :&: y) =>> f = (:&:) x $ f (x :&: y)
+
+instance Comonad (Product a) where
