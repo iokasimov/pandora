23b20f724dcfd4b1b1997c16609ebba1acf99a57
Murat Kasimov <iokasimov.m@gmail.com>
2019-01-19 01:18:49 +0300

    Add Pointable, Exctractable, Exclusive, Applicative and Alternative instances to Transformer paradigm

--- Paradigm/Basis/Functor/Transformer.hs
+++ Paradigm/Basis/Functor/Transformer.hs
@@ -3,8 +3,11 @@ module Paradigm.Basis.Functor.Transformer (T (..), type (:!:), up) where
 import Core.Composition ((:.:))
 import Core.Morphism ((.), ($))
 import Pattern.Functor.Covariant (Covariant ((<$>), comap))
-import Pattern.Functor.Extractable (Extractable (extract))
 import Pattern.Functor.Pointable (Pointable (point))
+import Pattern.Functor.Extractable (Extractable (extract))
+import Pattern.Functor.Exclusive (Exclusive (exclusive))
+import Pattern.Functor.Alternative (Alternative ((<+>)))
+import Pattern.Functor.Applicative (Applicative ((<*>), apply))
 import Pattern.Functor.Bindable (Bindable ((>>=), bind))
 import Pattern.Functor.Liftable (Liftable (lift))
 import Pattern.Functor.Lowerable (Lowerable (lower))
@@ -19,6 +22,18 @@ instance (Covariant t, Covariant u) => Covariant (T t u) where
 
 instance (Pointable t, Pointable u) => Pointable (T t u) where
 	point = T . point . point
+
+instance (Extractable t, Extractable u) => Extractable (T t u) where
+	extract = extract . extract . t
+
+instance (Covariant t, Exclusive u) => Exclusive (T t u) where
+	exclusive = T exclusive
+
+instance (Covariant t, Alternative u) => Alternative (T t u) where
+	T x <+> T y = T $ x <+> y
+
+instance (Applicative t, Applicative u) => Applicative (T t u) where
+	T f <*> T x = T $ apply <$> f <*> x
 
 instance Pointable t => Liftable (T t) where
 	lift x = T $ point <$> x
