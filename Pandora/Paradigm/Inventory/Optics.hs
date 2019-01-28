f2e799cb9f226e06859f553e3d1e78312490226f
Murat Kasimov <iokasimov.m@gmail.com>
2019-01-28 13:04:30 +0300

    Define lens composition

--- Pandora/Paradigm/Inventory/Optics.hs
+++ Pandora/Paradigm/Inventory/Optics.hs
@@ -1,12 +1,15 @@
-module Pandora.Paradigm.Inventory.Optics (Lens, view, set, over, (^.), (.~), (%~)) where
+module Pandora.Paradigm.Inventory.Optics (Lens, (|>), view, set, over, (^.), (.~), (%~)) where
 
-import Pandora.Core.Morphism ((.))
-import Pandora.Paradigm.Inventory.Storage (Storage, position, access, retrofit)
+import Pandora.Core.Morphism ((.), ($))
+import Pandora.Pattern.Functor.Covariant (Covariant ((<$)))
+import Pandora.Paradigm.Inventory.Storage (Storage (Storage), access, position, retrofit)
 import Pandora.Pattern.Functor.Extractable (Extractable (extract))
 
-infixr 9 .~, ^., %~
-
 type Lens t source target = source -> Storage target t source
+
+-- | Lens composition infix operator
+(|>) :: Covariant t => Lens t source between -> Lens t between target -> Lens t source target
+from |> to = \x -> ((<$) x . to) . position . from $ x
 
 -- | Get the target of a lens
 view :: Lens t a p -> a -> p
