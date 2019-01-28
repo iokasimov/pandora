ec05aee326d0ef63c1f8e482e05066e48c05daee
Murat Kasimov <iokasimov.m@gmail.com>
2019-01-28 13:47:11 +0300

    Instantiate Identity in Storage as Lens type synonymous

--- Pandora/Paradigm/Inventory/Optics.hs
+++ Pandora/Paradigm/Inventory/Optics.hs
@@ -1,36 +1,37 @@
 module Pandora.Paradigm.Inventory.Optics (Lens, (|>), view, set, over, (^.), (.~), (%~)) where
 
 import Pandora.Core.Morphism ((.), ($))
-import Pandora.Pattern.Functor.Covariant (Covariant ((<$)))
+import Pandora.Paradigm.Basis.Identity (Identity)
 import Pandora.Paradigm.Inventory.Storage (Storage (Storage), access, position, retrofit)
+import Pandora.Pattern.Functor.Covariant (Covariant ((<$)))
 import Pandora.Pattern.Functor.Extractable (Extractable (extract))
 
-type Lens t source target = source -> Storage target t source
+type Lens src tgt = src -> Storage tgt Identity src
 
 -- | Lens composition infix operator
-(|>) :: Covariant t => Lens t source between -> Lens t between target -> Lens t source target
+(|>) :: Lens src btw -> Lens btw tgt -> Lens src tgt
 from |> to = \x -> ((<$) x . to) . position . from $ x
 
 -- | Get the target of a lens
-view :: Lens t a p -> a -> p
+view :: Lens src tgt -> src -> tgt
 view lens = position . lens
 
 -- | Infix version of `view`
-(^.) :: Lens t a p -> a -> p
+(^.) :: Lens src tgt -> src -> tgt
 (^.) = view
 
 -- | Replace the target of a lens
-set :: Extractable t => Lens t a p -> p -> a -> a
+set :: Lens src tgt -> tgt -> src -> src
 set lens new = access new . lens
 
 -- | Infix version of `set`
-(.~) :: Extractable t => Lens t a p -> p -> a -> a
+(.~) :: Lens src tgt -> tgt -> src -> src
 lens .~ new = set lens new
 
 -- | Modify the target of a lens
-over :: Extractable t => Lens t a p -> (p -> p) -> a -> a
+over :: Lens src tgt -> (tgt -> tgt) -> src -> src
 over lens f = extract . retrofit f . lens
 
 -- | Infix version of `over`
-(%~) :: Extractable t => Lens t a p -> (p -> p) -> a -> a
+(%~) :: Lens src tgt -> (tgt -> tgt) -> src -> src
 lens %~ f = over lens f
