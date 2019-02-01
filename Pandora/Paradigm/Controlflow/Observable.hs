20042a1c28efbd5adad4899819bdbe7e23728e45
Murat Kasimov <iokasimov.m@gmail.com>
2019-02-01 17:04:13 +0300

    Return back Capture to Observable

--- Pandora/Paradigm/Controlflow/Observable.hs
+++ Pandora/Paradigm/Controlflow/Observable.hs
@@ -2,19 +2,20 @@ module Pandora.Paradigm.Controlflow.Observable (Observable, observe,
 	notify, follow, subscribe, watch, (.:~.), (.:~*), (*:~.), (*:~*)) where
 
 import Pandora.Core.Morphism ((.), ($))
-import Pandora.Paradigm.Basis.Capture (Capture (Capture, captured))
 import Pandora.Paradigm.Basis.Continuation (Continuation (Continuation, continue))
 import Pandora.Pattern.Functor.Applicative (Applicative (forever))
+
+newtype Capture r t a = Capture { captured :: t r }
 
 type Observable t a r = Continuation r (Capture r t) a
 
 -- | Make continuation observable
 observe :: Continuation r t a -> Observable t a r
-observe f = Continuation $ \h -> Capture $ continue f (captured . h)
+observe action = Continuation $ \h -> Capture $ continue action (captured . h)
 
 -- | Listen only first event, call back just once
 notify :: Observable t a r -> (a -> t r) -> t r
-notify r f = captured $ continue r (Capture . f)
+notify r action = captured $ continue r (Capture . action)
 
 -- | Infix version of 'notify'
 (.:~.) :: Observable t a r -> (a -> t r) -> t r
@@ -22,7 +23,7 @@ notify r f = captured $ continue r (Capture . f)
 
 -- | Listen only first event, call back forever
 follow :: Applicative t => Observable t a r -> (a -> t r) -> t r
-follow r f = captured $ continue r (Capture . forever . f)
+follow r action = captured $ continue r (Capture . forever . action)
 
 -- | Infix version of 'follow'
 (.:~*) :: Applicative t => Observable t a r -> (a -> t r) -> t r
@@ -30,7 +31,7 @@ follow r f = captured $ continue r (Capture . forever . f)
 
 -- | Listen all events from action, call back just once
 subscribe :: Applicative t => Observable t a r -> (a -> t r) -> t r
-subscribe r f = forever $ captured $ continue r (Capture . f)
+subscribe r action = forever $ captured $ continue r (Capture . action)
 
 -- | Infix version of 'subscribe'
 (*:~.) :: Applicative t => Observable t a r -> (a -> t r) -> t r
@@ -38,7 +39,7 @@ subscribe r f = forever $ captured $ continue r (Capture . f)
 
 -- | Listen all events from action, call back forever
 watch :: Applicative t => Observable t a r -> (a -> t r) -> t r
-watch r f = forever $ captured $ continue r (Capture . forever . f)
+watch r action = forever $ captured $ continue r (Capture . forever . action)
 
 -- | Infix version of 'watch'
 (*:~*) :: Applicative t => Observable t a r -> (a -> t r) -> t r
