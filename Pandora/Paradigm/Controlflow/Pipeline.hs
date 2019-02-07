8e1a2b063d6adf8500429a33ed47713cf9247127
Murat Kasimov <iokasimov.m@gmail.com>
2019-02-07 13:18:00 +0300

    Add comments to Pipeline module and bump to new version

--- Pandora/Paradigm/Controlflow/Pipeline.hs
+++ Pandora/Paradigm/Controlflow/Pipeline.hs
@@ -27,24 +27,30 @@ pause next ik = Producer $ \ok -> (pipe $ next ()) ik ok
 suspend :: (i -> Pipe i o r t a) -> Consumer o t r -> Consumer i t r
 suspend next ok = Consumer $ \v ik -> pipe (next v) ik ok
 
+-- | Take incoming value from pipeline
 await :: Pipeline i o t i r
 await = Continuation $ \next -> Pipe $ \i o -> produce i (suspend next o)
 
+-- | Give a value to the future consuming
 yield :: o -> Pipeline i o t () r
 yield v = Continuation $ \next -> Pipe $ \i o -> consume o v (pause next i)
 
+-- | Pipeline that does nothing
 finish :: Pointable t => Pipeline i o t () ()
 finish = Continuation $ \_ -> Pipe $ \_ _ -> point ()
 
+-- | Do some effectful computation within pipeline
 impact :: Bindable t => t a -> Pipeline i o t a a
 impact action = Continuation $ \next -> Pipe $ \i o -> action >>= \x -> pipe (next x) i o
 
+-- | Compose two pipelines into one
 (=*=) :: forall i e a o t . Pointable t => Pipeline i e t () () -> Pipeline e o t () () -> Pipeline i o t a ()
 p =*= q = Continuation $ \_ -> Pipe $ \i o -> pipe (continue q end) (pause (\() -> continue p end) i) o where
 
 	end :: b -> Pipe c d () t ()
 	end _ = Pipe $ \_ _ -> point ()
 
+-- | Run pipeline and get result
 pipeline :: Pointable t => Pipeline i o t r r -> t r
 pipeline p = pipe (continue p (\r -> Pipe $ \_ _ -> point r)) i o where
 
