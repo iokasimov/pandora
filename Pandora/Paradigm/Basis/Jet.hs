ce3bb21de53d665a5b4fe614ae847bcbd12e786d
Murat Kasimov <iokasimov.m@gmail.com>
2019-02-23 13:49:51 +0300

    Define Exctractable and Pointable instances for Jet datatype

--- Pandora/Paradigm/Basis/Jet.hs
+++ Pandora/Paradigm/Basis/Jet.hs
@@ -2,6 +2,9 @@ module Pandora.Paradigm.Basis.Jet (Jet (..)) where
 
 import Pandora.Core.Morphism ((.))
 import Pandora.Pattern.Functor.Covariant (Covariant ((<$>)), comap)
+import Pandora.Pattern.Functor.Exclusive (Exclusive (exclusive))
+import Pandora.Pattern.Functor.Pointable (Pointable (point))
+import Pandora.Pattern.Functor.Extractable (Extractable (extract))
 import Pandora.Pattern.Functor.Applicative (Applicative ((<*>)))
 import Pandora.Pattern.Functor.Traversable (Traversable ((->>), traverse))
 
@@ -12,3 +15,9 @@ instance Covariant t => Covariant (Jet t) where
 
 instance Traversable t => Traversable (Jet t) where
 	a :- as ->> f = (:-) <$> f a <*> (traverse . traverse) f as
+
+instance (forall t . Exclusive t) => Pointable (Jet t) where
+	point x = x :- exclusive
+
+instance Covariant t => Extractable (Jet t) where
+	extract (x :- _) = x
