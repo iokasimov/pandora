46f16eb2eaebe8eb43cb9812cd5c199eceff448b
Murat Kasimov <iokasimov.m@gmail.com>
2019-07-17 15:44:06 +0300

    Fix imports on Junction submodule

--- Pandora/Pattern.hs
+++ Pandora/Pattern.hs
@@ -1,4 +1,5 @@
 module Pandora.Pattern (module Exports) where
 
 import Pandora.Pattern.Object as Exports
+import Pandora.Pattern.Junction as Exports
 import Pandora.Pattern.Functor as Exports
---
+++ Pandora/Pattern/Junction/Schemes.hs
@@ -0,0 +1,7 @@
+module Pandora.Pattern.Junction.Schemes (module Exports) where
+
+import Pandora.Pattern.Junction.Schemes.UTU as Exports
+import Pandora.Pattern.Junction.Schemes.UT as Exports
+import Pandora.Pattern.Junction.Schemes.TUVW as Exports
+import Pandora.Pattern.Junction.Schemes.TUV as Exports
+import Pandora.Pattern.Junction.Schemes.TU as Exports
