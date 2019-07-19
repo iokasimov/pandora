dee8a1f3ebaa0c69ca61145180a67ce7a8391196
Murat Kasimov <iokasimov.m@gmail.com>
2019-07-19 21:11:42 +0300

    Define Traversable instance for Graph, return back loose function

--- Pandora/Paradigm/Structure/Graph.hs
+++ Pandora/Paradigm/Structure/Graph.hs
@@ -1,11 +1,13 @@
-module Pandora.Paradigm.Structure.Graph (Graph) where
+module Pandora.Paradigm.Structure.Graph (Graph, loose) where
 
 import Pandora.Core.Functor (type (:.:), type (><))
 import Pandora.Core.Morphism ((.), ($))
 import Pandora.Paradigm.Basis.Edges (Edges (Empty, Overlay))
 import Pandora.Paradigm.Basis.Twister (Twister ((:<)))
-import Pandora.Pattern.Junction.Composition (Composition (Primary, unwrap))
+import Pandora.Paradigm.Inventory.Stateful (fold)
 import Pandora.Pattern.Functor.Covariant (Covariant ((<$>), (<$$>)))
+import Pandora.Pattern.Functor.Traversable (Traversable ((->>), (->>>)))
+import Pandora.Pattern.Junction.Composition (Composition (Primary, unwrap))
 
 -- | Acyclic graph structure without loops
 newtype Graph a = Graph (Edges :.: Twister Edges >< a)
@@ -13,6 +15,13 @@ newtype Graph a = Graph (Edges :.: Twister Edges >< a)
 instance Covariant Graph where
 	f <$> Graph stack = Graph $ f <$$> stack
 
+instance Traversable Graph where
+	Graph stack ->> f = Graph <$> stack ->>> f
+
 instance Composition Graph where
 	type Primary Graph a = Edges :.: Twister Edges >< a
 	unwrap (Graph stack) = stack
+
+-- | Transform any traversable structure into all loose edges graph
+loose :: Traversable t => t a -> Graph a
+loose = Graph . fold Empty (\x -> Overlay . (:<) x)
