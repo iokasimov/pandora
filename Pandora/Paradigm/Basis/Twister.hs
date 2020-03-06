b22d4b2053ec658c94f1aa6afc4755acef0af23f
Murat Kasimov <iokasimov.m@gmail.com>
2020-03-06 16:16:27 +0300

    Define Lowerable instance for Twister

--- Pandora/Paradigm/Basis/Twister.hs
+++ Pandora/Paradigm/Basis/Twister.hs
@@ -13,6 +13,7 @@ import Pandora.Pattern.Functor.Bindable (Bindable ((>>=)))
 import Pandora.Pattern.Functor.Extendable (Extendable ((=>>), extend))
 import Pandora.Pattern.Functor.Monad (Monad)
 import Pandora.Pattern.Functor.Comonad (Comonad)
+import Pandora.Pattern.Functor.Lowerable (Lowerable (lower))
 import Pandora.Pattern.Functor.Divariant (($))
 import Pandora.Pattern.Object.Setoid (Setoid ((==)), (&&))
 import Pandora.Pattern.Object.Semigroup (Semigroup ((+)))
@@ -21,7 +22,7 @@ import Pandora.Pattern.Object.Monoid (Monoid (zero))
 data Twister t a = Twister a (t :. Twister t := a)
 
 instance Covariant t => Covariant (Twister t) where
-	f <$> Twister x xs = Twister (f x) (f <$$> xs)
+	f <$> Twister x xs = Twister (f x) $ f <$$> xs
 
 instance Avoidable t => Pointable (Twister t) where
 	point x = Twister x empty
@@ -45,6 +46,9 @@ instance (Avoidable t, Alternative t) => Monad (Twister t) where
 
 instance Covariant t => Comonad (Twister t) where
 
+instance Lowerable Twister where
+	lower (Twister _ xs) = extract <$> xs
+
 instance (Setoid a, forall b . Setoid b => Setoid (t b)) => Setoid (Twister t a) where
 	Twister x xs == Twister y ys = x == y && xs == ys
 
@@ -57,7 +61,7 @@ instance (Monoid a, forall b . Semigroup b => Monoid (t b)) => Monoid (Twister t
 untwist :: Twister t a -> (t :. Twister t) a
 untwist (Twister _ xs) = xs
 
-coiterate :: Covariant t => a |-> t -> a -> Twister t a
+coiterate :: Covariant t => a |-> t -> a |-> Twister t
 coiterate coalgebra x = Twister x $ coiterate coalgebra <$> coalgebra x
 
 section :: Comonad t => t ~> Twister t
