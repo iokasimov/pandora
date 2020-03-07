8905469aeb297890a8031986e6a32060d5912914
Murat Kasimov <iokasimov.m@gmail.com>
2020-03-07 10:09:22 +0300

    Use UT joint scheme for Graph data structure

--- Pandora/Paradigm/Structure/Specific/Graph.hs
+++ Pandora/Paradigm/Structure/Specific/Graph.hs
@@ -1,29 +1,27 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
 module Pandora.Paradigm.Structure.Specific.Graph (Graph, loose) where
 
-import Pandora.Core.Functor (type (:.), type (:=))
+import Pandora.Core.Functor (Variant (Co))
 import Pandora.Core.Transformation (type (~>))
-import Pandora.Paradigm.Basis.Edges (Edges (Empty, Overlay))
-import Pandora.Paradigm.Basis.Twister (Twister (Twister))
-import Pandora.Paradigm.Inventory.State (fold)
 import Pandora.Pattern.Category ((.))
 import Pandora.Pattern.Functor.Covariant (Covariant ((<$>), (<$$>)))
 import Pandora.Pattern.Functor.Divariant (($))
 import Pandora.Pattern.Functor.Traversable (Traversable ((->>), (->>>)))
-import Pandora.Paradigm.Controlflow.Joint.Interpreted (Interpreted (Primary, unwrap))
+import Pandora.Paradigm.Basis.Edges (Edges (Empty, Overlay))
+import Pandora.Paradigm.Basis.Twister (Twister (Twister))
+import Pandora.Paradigm.Controlflow.Joint.Schemes.UT (UT (UT))
+import Pandora.Paradigm.Inventory.State (fold)
 
--- | Acyclic graph structure without loops
-newtype Graph a = Graph (Edges :. Twister Edges := a)
+-- | Directed acyclic graph structure
+type Graph = UT 'Co 'Co (Twister Edges) Edges
 
-instance Covariant Graph where
-	f <$> Graph g = Graph $ f <$$> g
+instance Covariant (UT 'Co 'Co (Twister Edges) Edges) where
+	f <$> UT g = UT $ f <$$> g
 
-instance Traversable Graph where
-	Graph g ->> f = Graph <$> g ->>> f
-
-instance Interpreted Graph where
-	type Primary Graph a = Edges :. Twister Edges := a
-	unwrap (Graph g) = g
+instance Traversable (UT 'Co 'Co (Twister Edges) Edges) where
+	UT g ->> f = UT <$> g ->>> f
 
 -- | Transform any traversable structure into all loose edges graph
 loose :: Traversable t => t ~> Graph
-loose = Graph . fold Empty (\x -> Overlay . Twister x)
+loose = UT . fold Empty (\x -> Overlay . Twister x)
