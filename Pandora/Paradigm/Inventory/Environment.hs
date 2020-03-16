47a7e9ae3e639b558e88e2bb36aef81d0507cd7f
Murat Kasimov <iokasimov.m@gmail.com>
2020-03-16 16:06:16 +0300

    Define comonad transformer for Environment

--- Pandora/Paradigm/Inventory/Environment.hs
+++ Pandora/Paradigm/Inventory/Environment.hs
@@ -5,17 +5,24 @@ module Pandora.Paradigm.Inventory.Environment (Environment (..), Configured, env
 import Pandora.Core.Morphism ((!), (%))
 import Pandora.Paradigm.Controlflow.Joint.Interpreted (Interpreted (Primary, run))
 import Pandora.Paradigm.Controlflow.Joint.Transformer.Monadic (Monadic (lay, wrap), (:>) (TM))
+import Pandora.Paradigm.Controlflow.Joint.Transformer.Comonadic (Comonadic (flick, bring), (:<) (TC))
 import Pandora.Paradigm.Controlflow.Joint.Schematic (Schematic)
 import Pandora.Paradigm.Controlflow.Joint.Adaptable (Adaptable (adapt))
 import Pandora.Paradigm.Controlflow.Joint.Schemes.TU (TU (TU))
+import Pandora.Paradigm.Controlflow.Joint.Schemes.UT (UT (UT))
 import Pandora.Pattern.Category (identity, (.))
-import Pandora.Pattern.Functor.Covariant (Covariant ((<$>)))
+import Pandora.Pattern.Functor.Covariant (Covariant ((<$>), (<$$>)))
 import Pandora.Pattern.Functor.Pointable (Pointable (point))
-import Pandora.Pattern.Functor.Applicative (Applicative ((<*>)))
+import Pandora.Pattern.Functor.Extractable (Extractable (extract))
+import Pandora.Pattern.Functor.Applicative (Applicative ((<*>), (<**>)))
 import Pandora.Pattern.Functor.Distributive (Distributive ((>>-)))
 import Pandora.Pattern.Functor.Bindable (Bindable ((>>=)))
+import Pandora.Pattern.Functor.Extendable (Extendable ((=>>)))
 import Pandora.Pattern.Functor.Monad (Monad)
+import Pandora.Pattern.Functor.Comonad (Comonad)
 import Pandora.Pattern.Functor.Divariant (($))
+import Pandora.Pattern.Object.Monoid (Monoid (zero))
+import Pandora.Pattern.Object.Semigroup (Semigroup ((+)))
 
 newtype Environment e a = Environment (e -> a)
 
@@ -24,6 +31,9 @@ instance Covariant (Environment e) where
 
 instance Pointable (Environment e) where
 	point x = Environment (x !)
+
+instance Monoid e => Extractable (Environment e) where
+	extract (Environment x) = x zero
 
 instance Applicative (Environment e) where
 	f <*> x = Environment $ \e -> run f e $ run x e
@@ -36,11 +46,14 @@ instance Bindable (Environment e) where
 
 instance Monad (Environment e) where
 
-type instance Schematic Monad (Environment e) u = TU Covariant Covariant ((->) e) u
+instance Semigroup e => Extendable (Environment e) where
+	Environment x =>> f = Environment $ \e -> f $ Environment $ x . (e +)
 
 instance Interpreted (Environment e) where
 	type Primary (Environment e) a = (->) e a
 	run (Environment x) = x
+
+type instance Schematic Monad (Environment e) u = TU Covariant Covariant ((->) e) u
 
 instance Monadic (Environment e) where
 	lay = TM . TU . (!)
@@ -49,16 +62,31 @@ instance Monadic (Environment e) where
 type Configured e = Adaptable (Environment e)
 
 instance Covariant u => Covariant (TU Covariant Covariant ((->) e) u) where
-	f <$> TU x = TU $ \r -> f <$> x r
+	f <$> TU x = TU $ f <$$> x
 
 instance (Covariant u, Pointable u) => Pointable (TU Covariant Covariant ((->) e) u) where
 	point = TU . point . point
 
 instance Applicative u => Applicative (TU Covariant Covariant ((->) e) u) where
-	TU f <*> TU x = TU $ \r -> f r <*> x r
+	TU f <*> TU x = TU $ f <**> x
 
 instance Bindable u => Bindable (TU Covariant Covariant ((->) e) u) where
 	TU x >>= f = TU $ \e -> x e >>= ($ e) . run . f
 
 env :: Configured e t => t e
 env = adapt $ Environment identity
+
+type instance Schematic Comonad (Environment e) u = UT Covariant Covariant ((->) e) u
+
+instance Monoid e => Comonadic (Environment e) where
+	flick (TC (UT x)) = extract <$> x
+	bring (TC (UT x)) = Environment . extract $ x
+
+instance Covariant u => Covariant (UT Covariant Covariant ((->) e) u) where
+	f <$> UT x = UT $ f <$$> x
+
+instance Applicative u => Applicative (UT Covariant Covariant ((->) e) u) where
+	UT f <*> UT x = UT $ f <**> x
+
+instance (Semigroup e, Extendable u) => Extendable (UT Covariant Covariant ((->) e) u) where
+	UT x =>> f = UT $ x =>> (\x' e -> f . UT . (<$>) (. (e +)) $ x')
--- Pandora/Pattern/Functor/Extractable.hs
+++ Pandora/Pattern/Functor/Extractable.hs
@@ -2,6 +2,10 @@ module Pandora.Pattern.Functor.Extractable (Extractable (..)) where
 
 import Pandora.Core.Functor (type (<-|))
 import Pandora.Pattern.Functor.Covariant (Covariant)
+import Pandora.Pattern.Object.Monoid (Monoid (zero))
 
 class Covariant t => Extractable t where
 	extract :: a <-| t
+
+instance Monoid e => Extractable ((->) e) where
+	extract f = f zero
