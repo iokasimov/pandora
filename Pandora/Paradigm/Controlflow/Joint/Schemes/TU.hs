cff4eaafafa4f4ebaa1e5e864cf7d6731a184986
Murat Kasimov <iokasimov.m@gmail.com>
2020-03-23 17:41:02 +0300

    Define Hoistable instance for TU

--- Pandora/Paradigm/Controlflow/Joint/Schemes/TU.hs
+++ Pandora/Paradigm/Controlflow/Joint/Schemes/TU.hs
@@ -1,6 +1,9 @@
 module Pandora.Paradigm.Controlflow.Joint.Schemes.TU (TU (..)) where
 
 import Pandora.Core.Functor (type (:.), type (:=))
+import Pandora.Pattern.Functor.Covariant (Covariant ((<$>)))
+import Pandora.Pattern.Functor.Divariant (($))
+import Pandora.Pattern.Transformer.Hoistable (Hoistable (hoist))
 import Pandora.Paradigm.Controlflow.Joint.Interpreted (Interpreted (Primary, run))
 
 newtype TU ct cu t u a = TU (t :. u := a)
@@ -8,3 +11,6 @@ newtype TU ct cu t u a = TU (t :. u := a)
 instance Interpreted (TU ct cu t u) where
 	type Primary (TU ct cu t u) a = t :. u := a
 	run (TU x) = x
+
+instance Covariant t => Hoistable (TU Covariant Covariant t) where
+	hoist f (TU x) = TU $ f <$> x
