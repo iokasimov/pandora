7c8ffecf1c332241af79ae86cad09c9f6dcef083
Murat Kasimov <iokasimov.m@gmail.com>
2020-03-24 18:31:20 +0300

    Define Lifltable instances for UT and TU joint schemes

--- Pandora/Paradigm/Controlflow/Joint/Schemes/TU.hs
+++ Pandora/Paradigm/Controlflow/Joint/Schemes/TU.hs
@@ -1,8 +1,11 @@
 module Pandora.Paradigm.Controlflow.Joint.Schemes.TU (TU (..)) where
 
 import Pandora.Core.Functor (type (:.), type (:=))
+import Pandora.Pattern.Category ((.))
 import Pandora.Pattern.Functor.Covariant (Covariant ((<$>)))
+import Pandora.Pattern.Functor.Pointable (Pointable (point))
 import Pandora.Pattern.Functor.Divariant (($))
+import Pandora.Pattern.Transformer.Liftable (Liftable (lift))
 import Pandora.Pattern.Transformer.Hoistable (Hoistable (hoist))
 import Pandora.Paradigm.Controlflow.Joint.Interpreted (Interpreted (Primary, run))
 
@@ -11,6 +14,9 @@ newtype TU ct cu t u a = TU (t :. u := a)
 instance Interpreted (TU ct cu t u) where
 	type Primary (TU ct cu t u) a = t :. u := a
 	run (TU x) = x
+
+instance Pointable t => Liftable (TU Covariant Covariant t) where
+	lift = TU . point
 
 instance Covariant t => Hoistable (TU Covariant Covariant t) where
 	hoist f (TU x) = TU $ f <$> x
--- Pandora/Paradigm/Controlflow/Joint/Schemes/UT.hs
+++ Pandora/Paradigm/Controlflow/Joint/Schemes/UT.hs
@@ -1,6 +1,10 @@
 module Pandora.Paradigm.Controlflow.Joint.Schemes.UT (UT (..)) where
 
 import Pandora.Core.Functor (type (:.), type (:=))
+import Pandora.Pattern.Functor.Covariant (Covariant ((<$>)))
+import Pandora.Pattern.Functor.Pointable (Pointable (point))
+import Pandora.Pattern.Functor.Divariant (($))
+import Pandora.Pattern.Transformer.Liftable (Liftable (lift))
 import Pandora.Paradigm.Controlflow.Joint.Interpreted (Interpreted (Primary, run))
 
 newtype UT ct cu t u a = UT (u :. t := a)
@@ -8,3 +12,6 @@ newtype UT ct cu t u a = UT (u :. t := a)
 instance Interpreted (UT ct cu t u) where
 	type Primary (UT ct cu t u) a = u :. t := a
 	run (UT x) = x
+
+instance Pointable t => Liftable (UT Covariant Covariant t) where
+	lift x = UT $ point <$> x
