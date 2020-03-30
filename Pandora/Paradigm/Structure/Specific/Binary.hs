58b12332d754ad90d33d7e965ff4d277fa863b47
Murat Kasimov <iokasimov.m@gmail.com>
2020-03-30 22:13:31 +0300

    Binary trees can be empty

--- Pandora/Paradigm/Structure/Specific/Binary.hs
+++ Pandora/Paradigm/Structure/Specific/Binary.hs
@@ -1,20 +1,34 @@
 module Pandora.Paradigm.Structure.Specific.Binary (Binary, insert) where
 
-import Pandora.Core.Morphism ((&))
-import Pandora.Paradigm.Basis.Wye (Wye (End, Left, Right, Both))
-import Pandora.Paradigm.Basis.Twister (Twister (Twister))
+import Pandora.Core.Morphism ((&), (%))
 import Pandora.Pattern.Category ((.))
+import Pandora.Pattern.Functor.Covariant (Covariant ((<$>)))
 import Pandora.Pattern.Functor.Divariant (($))
 import Pandora.Pattern.Object.Chain (Chain ((<=>)), order)
+import Pandora.Paradigm.Basis.Maybe (Maybe (Just, Nothing))
+import Pandora.Paradigm.Basis.Wye (Wye (End, Left, Right, Both))
+import Pandora.Paradigm.Basis.Twister (Twister (Twister))
+import Pandora.Paradigm.Controlflow.Joint.Schemes.UT (UT (UT))
+import Pandora.Paradigm.Controlflow.Joint.Interpreted (run)
 
-type Binary = Twister Wye
+-- type Binary = Twister Wye
+type Binary = UT Covariant Covariant (Twister Wye) Maybe
 
 insert :: Chain a => a -> Binary a -> Binary a
-insert x (Twister y End) = x <=> y & order (Twister y . Right $ Twister x End)
-	(Twister y . Right $ Twister x End) (Twister y . Left $ Twister x End)
-insert x (Twister y (Left ls)) = x <=> y & order (Twister y . Both ls $ Twister x End)
-	(Twister y $ Both ls $ Twister x End) (Twister y . Left $ insert x ls)
-insert x (Twister y (Right rs)) = x <=> y & order (Twister y . Right $ insert x rs)
-	(Twister y $ Right (insert x rs)) (Twister y $ Both (Twister x End) rs)
-insert x (Twister y (Both ls rs)) = x <=> y & order (Twister y . Both ls $ insert x rs)
-	(Twister y . Both ls $ insert x rs) (Twister y $ Both (insert x ls) rs)
+insert x (UT Nothing) = UT $ Just $ Twister x End
+insert x (UT (Just (Twister y End))) = x <=> y & order
+	(UT . Just . Twister y . Right $ Twister x End)
+	(UT . Just . Twister y . Right $ Twister x End)
+	(UT . Just . Twister y . Left $ Twister x End)
+insert x (UT (Just (Twister y (Left ls)))) = x <=> y & order
+	(UT . Just . Twister y . Both ls $ Twister x End)
+	(UT . Just . Twister y $ Both ls $ Twister x End)
+	(UT $ Twister y . Left <$> run (insert x . UT . Just $ ls))
+insert x (UT (Just (Twister y (Right rs)))) = x <=> y & order
+	(UT $ Twister y . Right <$> run (insert x . UT . Just $ rs))
+	(UT $ Twister y . Right <$> run (insert x . UT . Just $ rs))
+	(UT . Just . Twister y $ Both (Twister x End) rs)
+insert x (UT (Just (Twister y (Both ls rs)))) = x <=> y & order
+	(UT $ Twister y . Both ls <$> run (insert x . UT . Just $ rs))
+	(UT $ Twister y . Both ls <$> run (insert x . UT . Just $ rs))
+	(UT $ Twister y . Both % rs <$> (run . insert x . UT . Just $ ls))
