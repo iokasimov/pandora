141b5d58909cf34d39fc6673cf74e9f7449ca30d
Murat Kasimov <iokasimov.m@gmail.com>
2020-03-30 22:18:02 +0300

    Define Covariant and Traversable instances for Binary trees

--- Pandora/Paradigm/Structure/Specific/Binary.hs
+++ Pandora/Paradigm/Structure/Specific/Binary.hs
@@ -1,8 +1,11 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
 module Pandora.Paradigm.Structure.Specific.Binary (Binary, insert) where
 
 import Pandora.Core.Morphism ((&), (%))
 import Pandora.Pattern.Category ((.))
-import Pandora.Pattern.Functor.Covariant (Covariant ((<$>)))
+import Pandora.Pattern.Functor.Covariant (Covariant ((<$>), (<$$>)))
+import Pandora.Pattern.Functor.Traversable (Traversable ((->>), (->>>)))
 import Pandora.Pattern.Functor.Divariant (($))
 import Pandora.Pattern.Object.Chain (Chain ((<=>)), order)
 import Pandora.Paradigm.Basis.Maybe (Maybe (Just, Nothing))
@@ -11,8 +14,13 @@ import Pandora.Paradigm.Basis.Twister (Twister (Twister))
 import Pandora.Paradigm.Controlflow.Joint.Schemes.UT (UT (UT))
 import Pandora.Paradigm.Controlflow.Joint.Interpreted (run)
 
--- type Binary = Twister Wye
 type Binary = UT Covariant Covariant (Twister Wye) Maybe
+
+instance Covariant Binary where
+	f <$> UT g = UT $ f <$$> g
+
+instance Traversable Binary where
+	UT g ->> f = UT <$> g ->>> f
 
 insert :: Chain a => a -> Binary a -> Binary a
 insert x (UT Nothing) = UT $ Just $ Twister x End
