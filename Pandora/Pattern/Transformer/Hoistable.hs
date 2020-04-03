50bcae320f61db243e2fc074d1c5c34703b79166
Murat Kasimov <iokasimov.m@gmail.com>
2020-04-03 20:15:54 +0300

    Provide laws to Hoistable typeclass

--- Pandora/Pattern/Transformer/Hoistable.hs
+++ Pandora/Pattern/Transformer/Hoistable.hs
@@ -2,5 +2,11 @@ module Pandora.Pattern.Transformer.Hoistable (Hoistable (..)) where
 
 import Pandora.Core.Functor (type (~>))
 
+{- |
+> When providing a new instance, you should ensure it satisfies one law:
+> * Identity morphism: hoist identity ≡ identity
+> * Interpreted of morphisms: hoist (f . g) ≡ hoist f . hoist g
+-}
+
 class Hoistable t where
 	hoist :: u ~> v -> t u ~> t v
