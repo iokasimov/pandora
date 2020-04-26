242809329d78c1f716a6ac7487b39918e491df95
Murat Kasimov <iokasimov.m@gmail.com>
2020-04-26 14:29:25 +0300

    Define Adjunction between TU and UT, UT and TU, UT and UT schemes

--- Pandora/Paradigm/Controlflow/Joint/Schemes.hs
+++ Pandora/Paradigm/Controlflow/Joint/Schemes.hs
@@ -18,3 +18,18 @@ instance (Covariant (TU Covariant Covariant v t), Covariant (TU Covariant Covari
 	=> Adjoint (TU Covariant Covariant v t) (TU Covariant Covariant u w) where
 		TU y |- g = y |- (|- run . g)
 		x -| f = TU $ x -| (-| f . TU)
+
+instance (Covariant (TU Covariant Covariant v t), Covariant (UT Covariant Covariant w u), Adjoint t u, Adjoint v w)
+	=> Adjoint (TU Covariant Covariant v t) (UT Covariant Covariant w u) where
+		TU y |- g = y |- (|- run . g)
+		x -| f = UT $ x -| (-| f . TU)
+
+instance (Covariant (UT Covariant Covariant t v), Covariant (UT Covariant Covariant w u), Adjoint t u, Adjoint v w)
+	=> Adjoint (UT Covariant Covariant t v) (UT Covariant Covariant w u) where
+		UT y |- g = y |- (|- run . g)
+		x -| f = UT $ x -| (-| f . UT)
+
+instance (Covariant (UT Covariant Covariant t v), Covariant (TU Covariant Covariant w u) , Adjoint v u, Adjoint t w)
+	=> Adjoint (UT Covariant Covariant t v) (TU Covariant Covariant w u) where
+		UT y |- g = y |- (|- run . g)
+		x -| f = TU $ x -| (-| f . UT)
