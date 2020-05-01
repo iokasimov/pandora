0bee05c579ebd9473bb3171147700b123ad30f39
Murat Kasimov <iokasimov.m@gmail.com>
2020-05-01 11:59:18 +0300

    Define Nonempty instance for Rose tree

--- Pandora/Paradigm/Structure/Rose.hs
+++ Pandora/Paradigm/Structure/Rose.hs
@@ -3,7 +3,10 @@ module Pandora.Paradigm.Structure.Rose (Rose) where
 import Pandora.Pattern.Functor.Covariant (Covariant)
 import Pandora.Paradigm.Primary.Functor.Maybe (Maybe)
 import Pandora.Paradigm.Primary.Transformer.Construction (Construction)
-import Pandora.Paradigm.Structure.Stack (Stack)
 import Pandora.Paradigm.Controlflow.Joint.Schemes.TU (TU)
+import Pandora.Paradigm.Structure.Stack (Stack)
+import Pandora.Paradigm.Structure.Variation.Nonempty (Nonempty)
 
 type Rose = TU Covariant Covariant Maybe (Construction Stack)
+
+type instance Nonempty Rose = Construction Stack
