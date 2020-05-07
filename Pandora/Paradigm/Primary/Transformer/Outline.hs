8ba22be7eb2245d6110b75161fd1f060b4731426
Murat Kasimov <iokasimov.m@gmail.com>
2020-05-07 20:01:42 +0300

    Define Liftable, Hostable and Interpreted instances for Outline

--- Pandora/Paradigm/Primary/Transformer/Outline.hs
+++ Pandora/Paradigm/Primary/Transformer/Outline.hs
@@ -1,12 +1,15 @@
 module Pandora.Paradigm.Primary.Transformer.Outline (Outline (..)) where
 
 import Pandora.Core.Morphism ((%))
-import Pandora.Pattern.Category ((.))
+import Pandora.Pattern.Category (identity, (.))
 import Pandora.Pattern.Functor.Covariant (Covariant ((<$>)))
 import Pandora.Pattern.Functor.Pointable (Pointable (point))
 import Pandora.Pattern.Functor.Extractable (Extractable (extract))
 import Pandora.Pattern.Functor.Applicative (Applicative ((<*>)))
 import Pandora.Pattern.Functor.Divariant (($))
+import Pandora.Pattern.Transformer.Liftable (Liftable (lift))
+import Pandora.Pattern.Transformer.Hoistable (Hoistable (hoist))
+import Pandora.Paradigm.Controlflow.Joint.Interpreted (Interpreted (Primary, run))
 
 data Outline t a where
 	Line :: a -> Outline t a
@@ -26,3 +29,15 @@ instance Extractable t => Extractable (Outline t) where
 instance Applicative (Outline f) where
 	Line f <*> y = f <$> y
 	Outlined x y <*> z = Outlined x ((%) <$> y <*> z)
+
+instance Liftable Outline where
+	lift t = Outlined t (Line identity)
+
+instance Hoistable Outline where
+	hoist f (Line x) = Line x
+	hoist f (Outlined x y) = Outlined (f x) (hoist f y)
+
+instance (Pointable t, Applicative t) => Interpreted (Outline t) where
+	type Primary (Outline t) a = t a
+	run (Line x) = point x
+	run (Outlined t f) = run f <*> t
