fedcd5f8824eca14d9fbb6b8520e6a430458cb63
Murat Kasimov <iokasimov.m@gmail.com>
2020-05-24 11:04:04 +0300

    Define Bindable instance for Tap

--- Pandora/Paradigm/Primary/Transformer/Tap.hs
+++ Pandora/Paradigm/Primary/Transformer/Tap.hs
@@ -6,9 +6,11 @@ import Pandora.Pattern.Functor.Covariant (Covariant ((<$>)))
 import Pandora.Pattern.Functor.Avoidable (Avoidable (empty))
 import Pandora.Pattern.Functor.Pointable (Pointable (point))
 import Pandora.Pattern.Functor.Extractable (Extractable (extract))
+import Pandora.Pattern.Functor.Alternative (Alternative ((<+>)))
 import Pandora.Pattern.Functor.Applicative (Applicative ((<*>)))
 import Pandora.Pattern.Functor.Traversable (Traversable ((->>)))
 import Pandora.Pattern.Functor.Extendable (Extendable ((=>>)))
+import Pandora.Pattern.Functor.Bindable (Bindable ((>>=)))
 import Pandora.Pattern.Functor.Divariant (($))
 import Pandora.Pattern.Transformer.Lowerable (Lowerable (lower))
 import Pandora.Pattern.Transformer.Hoistable (Hoistable (hoist))
@@ -29,6 +31,9 @@ instance Applicative t => Applicative (Tap t) where
 
 instance Traversable t => Traversable (Tap t) where
 	Tap x xs ->> f = Tap <$> f x <*> xs ->> f
+
+instance (Extractable t, Alternative t, Bindable t) => Bindable (Tap t) where
+	Tap x xs >>= f = case f x of Tap y ys -> Tap y $ ys <+> (xs >>= lower . f)
 
 instance (Extractable t, Extendable t) => Extendable (Tap t) where
 	x =>> f = Tap (f x) $ lower x =>> f . Tap (extract x)
