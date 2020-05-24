e1e38850d7257c571fa19d017da5b338fc64b5d9
Murat Kasimov <iokasimov.m@gmail.com>
2020-05-24 10:09:04 +0300

    Define Lowerable and Hoistable instances for Tap

--- Pandora/Paradigm/Primary/Transformer/Tap.hs
+++ Pandora/Paradigm/Primary/Transformer/Tap.hs
@@ -1,6 +1,5 @@
-module Pandora.Paradigm.Primary.Transformer.Tap (Tap (..), context) where
+module Pandora.Paradigm.Primary.Transformer.Tap (Tap (..)) where
 
-import Pandora.Core.Functor (type (~>))
 import Pandora.Core.Morphism ((%))
 import Pandora.Pattern.Category ((.))
 import Pandora.Pattern.Functor.Covariant (Covariant ((<$>)))
@@ -9,8 +8,10 @@ import Pandora.Pattern.Functor.Pointable (Pointable (point))
 import Pandora.Pattern.Functor.Extractable (Extractable (extract))
 import Pandora.Pattern.Functor.Applicative (Applicative ((<*>)))
 import Pandora.Pattern.Functor.Traversable (Traversable ((->>)))
-import Pandora.Pattern.Functor.Extendable (Extendable ((=>>), extend))
+import Pandora.Pattern.Functor.Extendable (Extendable ((=>>)))
 import Pandora.Pattern.Functor.Divariant (($))
+import Pandora.Pattern.Transformer.Lowerable (Lowerable (lower))
+import Pandora.Pattern.Transformer.Hoistable (Hoistable (hoist))
 
 data Tap t a = Tap a (t a)
 
@@ -29,8 +30,11 @@ instance Applicative t => Applicative (Tap t) where
 instance Traversable t => Traversable (Tap t) where
 	Tap x xs ->> f = Tap <$> f x <*> xs ->> f
 
-instance Extendable t => Extendable (Tap t) where
-	x =>> f = Tap (f x) $ context x =>> f . Tap (extract x)
+instance (Extractable t, Extendable t) => Extendable (Tap t) where
+	x =>> f = Tap (f x) $ lower x =>> f . Tap (extract x)
 
-context :: Tap t ~> t
-context (Tap _ xs) = xs
+instance Lowerable Tap where
+	lower (Tap _ xs) = xs
+
+instance Hoistable Tap where
+	hoist f (Tap x xs) = Tap x $ f xs
