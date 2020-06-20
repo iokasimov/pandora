5889201134700c67b1f0c5d6c89ae6ca920ba8e3
Murat Kasimov <iokasimov.m@gmail.com>
2020-06-20 11:45:01 +0300

    Fix imports: put Ability module in Structure instead of submodules of Ability

--- Pandora/Paradigm/Structure.hs
+++ Pandora/Paradigm/Structure.hs
@@ -1,7 +1,6 @@
 module Pandora.Paradigm.Structure (module Exports) where
 
-import Pandora.Paradigm.Structure.Ability.Substructure as Exports
-import Pandora.Paradigm.Structure.Ability.Nonempty as Exports
+import Pandora.Paradigm.Structure.Ability as Exports
 import Pandora.Paradigm.Structure.Rose as Exports
 import Pandora.Paradigm.Structure.Binary as Exports
 import Pandora.Paradigm.Structure.Stack as Exports
