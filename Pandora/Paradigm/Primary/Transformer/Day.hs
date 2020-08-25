241df71c595b353cff6fb8994db8563e2e0c2cdc
Murat Kasimov <iokasimov.m@gmail.com>
2020-08-25 07:51:38 +0300

    Define Extractable and Extendable instances for Day convolution

--- Pandora/Paradigm/Primary/Transformer/Day.hs
+++ Pandora/Paradigm/Primary/Transformer/Day.hs
@@ -3,7 +3,9 @@ module Pandora.Paradigm.Primary.Transformer.Day where
 import Pandora.Pattern.Category (($))
 import Pandora.Pattern.Functor.Covariant (Covariant ((<$>)), (.|..))
 import Pandora.Pattern.Functor.Pointable (Pointable (point))
+import Pandora.Pattern.Functor.Extractable (Extractable (extract))
 import Pandora.Pattern.Functor.Applicative (Applicative ((<*>)))
+import Pandora.Pattern.Functor.Extendable (Extendable ((=>>)))
 import Pandora.Paradigm.Primary.Functor.Product (Product ((:*:)))
 
 data Day t u a = forall b c . Day (t b) (u c) (b -> c -> a)
@@ -17,3 +19,9 @@ instance (Pointable t, Pointable u) => Pointable (Day t u) where
 instance (Applicative t, Applicative u) => Applicative (Day t u) where
 	Day tb uc bcad <*> Day vb wc bca = Day ((:*:) <$> tb <*> vb) ((:*:) <$> uc <*> wc)
 		$ \(b :*: b') (c :*: c') -> bcad b c $ bca b' c'
+
+instance (Extractable t, Extractable u) => Extractable (Day t u) where
+	extract (Day tb uc bcad) = bcad (extract tb) (extract uc)
+
+instance (Extendable t, Extendable u) => Extendable (Day t u) where
+	day@(Day tb uc _) =>> f = Day tb uc (\_ _ -> f day)
