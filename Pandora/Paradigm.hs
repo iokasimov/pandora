b761e8bf2e83b44ac0a4babefa48814f2ef6b749
Murat Kasimov <iokasimov.m@gmail.com>
2020-09-24 12:00:05 +0300

    Add Schemes export to Pandora.Paradigm module

--- Pandora/Paradigm.hs
+++ Pandora/Paradigm.hs
@@ -4,3 +4,4 @@ import Pandora.Paradigm.Structure as Exports
 import Pandora.Paradigm.Inventory as Exports
 import Pandora.Paradigm.Controlflow as Exports
 import Pandora.Paradigm.Primary as Exports
+import Pandora.Paradigm.Schemes as Exports
