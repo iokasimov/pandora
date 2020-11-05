f0a4a91b658f13589bc529e27cc93d4747290fd5
Murat Kasimov <iokasimov.m@gmail.com>
2020-11-05 10:42:29 +0300

    Fix member and subset methods of Set interface

--- Pandora/Paradigm/Structure/Interface/Set.hs
+++ Pandora/Paradigm/Structure/Interface/Set.hs
@@ -1,16 +1,18 @@
 module Pandora.Paradigm.Structure.Interface.Set where
 
-import Pandora.Core.Morphism ((!))
-import Pandora.Pattern.Category ((.))
+import Pandora.Core.Morphism ((!), (%))
+import Pandora.Pattern.Category ((.), ($))
+import Pandora.Pattern.Functor.Extractable (extract)
 import Pandora.Pattern.Functor.Traversable (Traversable ((->>)))
 import Pandora.Pattern.Object.Setoid (Setoid ((==), (/=)))
 import Pandora.Paradigm.Primary.Functor.Maybe (Maybe (Nothing), maybe)
 import Pandora.Paradigm.Primary.Functor.Predicate (Predicate (Predicate))
 import Pandora.Paradigm.Primary.Object.Boolean (Boolean (True, False))
-import Pandora.Paradigm.Inventory.State (find)
+import Pandora.Paradigm.Inventory.State (State, find)
+import Pandora.Paradigm.Controlflow (run)
 
--- member :: (Traversable t, Setoid a) => a -> t a -> Boolean
--- member x = maybe False (True !) . find (Predicate (== x))
---
--- subset :: (Traversable t, Setoid a) => t a -> t a -> Boolean
--- subset ss s = Nothing /= (ss ->> \x -> find (Predicate (== x)) s)
+member :: forall t a . (Traversable t, Setoid a) => a -> t a -> Boolean
+member x = maybe False (True !) . extract . run @(State (Maybe a)) % Nothing . find (Predicate (== x))
+
+subset :: forall t a . (Traversable t, Setoid a, Setoid (t a)) => t a -> t a -> Boolean
+subset ss s = Nothing /= (ss ->> \x -> extract . run @(State (Maybe a)) % Nothing $ find (Predicate (== x)) s)
