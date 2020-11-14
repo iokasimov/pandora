02df594bb2bd2b79a2e3aeb9ac1eb9a22832346d
Murat Kasimov <iokasimov.m@gmail.com>
2020-11-14 13:13:06 +0300

    Define Zipper for Stream

--- Pandora/Paradigm/Structure/Stream.hs
+++ Pandora/Paradigm/Structure/Stream.hs
@@ -1,10 +1,16 @@
 module Pandora.Paradigm.Structure.Stream where
 
 import Pandora.Pattern.Category ((.), ($))
+import Pandora.Paradigm.Primary.Functor.Delta (Delta ((:^:)))
 import Pandora.Paradigm.Primary.Functor.Identity (Identity (Identity))
 import Pandora.Paradigm.Primary.Transformer.Construction (Construction (Construct))
+import Pandora.Paradigm.Primary.Transformer.Tap (Tap (Tap))
+import Pandora.Paradigm.Structure.Ability.Zipper (Zipper)
+import Pandora.Paradigm.Schemes.TU (TU (TU), type (<:.>))
 
 type Stream = Construction Identity
 
 repeat :: a -> Stream a
 repeat x = Construct x . Identity $ repeat x
+
+type instance Zipper Stream = Tap (Delta <:.> Stream)
