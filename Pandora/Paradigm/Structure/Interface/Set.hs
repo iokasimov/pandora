d70df16e091b4d211debf6742687d19235e247bd
Murat Kasimov <iokasimov.m@gmail.com>
2020-11-17 07:13:21 +0300

    Return subset method of Set

--- Pandora/Paradigm/Structure/Interface/Set.hs
+++ Pandora/Paradigm/Structure/Interface/Set.hs
@@ -2,19 +2,15 @@ module Pandora.Paradigm.Structure.Interface.Set where
 
 import Pandora.Core.Morphism ((!), (%))
 import Pandora.Pattern.Category ((.))
-import Pandora.Pattern.Functor.Extractable (extract)
 import Pandora.Pattern.Functor.Traversable (Traversable ((->>)))
 import Pandora.Pattern.Object.Setoid (Setoid ((/=)))
 import Pandora.Paradigm.Primary.Functor.Maybe (Maybe (Nothing), maybe)
 import Pandora.Paradigm.Primary.Functor.Predicate (equate)
 import Pandora.Paradigm.Primary.Object.Boolean (Boolean (True, False))
-import Pandora.Paradigm.Inventory.State (State)
-import Pandora.Paradigm.Controlflow (run)
 import Pandora.Paradigm.Structure.Ability.Monotonic (Monotonic, find)
 
 member :: (Setoid a, Monotonic e a) => a -> e -> Boolean
 member x = maybe False (True !) . find (equate x)
 
--- subset :: forall t a . (Traversable t, Setoid a, Setoid (t a)) => t a -> t a -> Boolean
--- subset ss s = let result = extract . run @(State (Maybe a)) % Nothing
--- 	in Nothing /= (ss ->> result . find % s . equate)
+subset :: (Monotonic (t a) a, Traversable t, Setoid a, Setoid (t a)) => t a -> t a -> Boolean
+subset ss s = Nothing /= (ss ->> find % s . equate)
