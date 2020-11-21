4875ac73dc241da9481c81d747194f92514ec354
Murat Kasimov <iokasimov.m@gmail.com>
2020-11-21 11:52:43 +0300

    Define Semigroup, Ringoid, Monoid and Quasiring instances for Natural numbers

--- Pandora/Paradigm/Primary/Object/Natural.hs
+++ Pandora/Paradigm/Primary/Object/Natural.hs
@@ -1,3 +1,23 @@
 module Pandora.Paradigm.Primary.Object.Natural where
 
+import Pandora.Pattern.Category (($))
+import Pandora.Pattern.Object.Semigroup (Semigroup ((+)))
+import Pandora.Pattern.Object.Ringoid (Ringoid ((*)))
+import Pandora.Pattern.Object.Monoid (Monoid (zero))
+import Pandora.Pattern.Object.Quasiring (Quasiring (one))
+
 data Natural = Zero | Natural Natural
+
+instance Semigroup Natural where
+	Zero + m = m
+	Natural n + m = Natural $ n + m
+
+instance Ringoid Natural where
+	Zero * m = Zero
+	Natural n * m = m + n * m
+
+instance Monoid Natural where
+	zero = Zero
+
+instance Quasiring Natural where
+	one = Natural Zero
