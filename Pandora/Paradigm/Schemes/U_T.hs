3ba50fd87e9628f00c3cde1ad04131c3a0fe1c2c
Murat Kasimov <iokasimov.m@gmail.com>
2020-11-24 18:17:18 +0300

    Create type synonymous operator for U_T scheme

--- Pandora/Paradigm/Schemes/U_T.hs
+++ Pandora/Paradigm/Schemes/U_T.hs
@@ -1,9 +1,16 @@
 module Pandora.Paradigm.Schemes.U_T where
 
+import Pandora.Pattern.Functor.Covariant (Covariant)
+import Pandora.Pattern.Functor.Contravariant (Contravariant)
 import Pandora.Paradigm.Primary.Functor.Product (type (:*:))
 import Pandora.Paradigm.Controlflow.Effect.Interpreted (Interpreted (Primary, run))
 
 newtype U_T ct cu t u a = U_T (u a :*: t a)
+
+type (<.:.>) = U_T Covariant Covariant
+type (>.:.>) = U_T Contravariant Covariant
+type (<.:.<) = U_T Covariant Contravariant
+type (>.:.<) = U_T Contravariant Contravariant
 
 instance Interpreted (U_T ct cu t u) where
 	type Primary (U_T ct cu t u) a = u a :*: t a
