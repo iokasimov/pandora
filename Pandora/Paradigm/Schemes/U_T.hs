03084d279134ae6491ae1d4e615da34fe67ed2d8
Murat Kasimov <iokasimov.m@gmail.com>
2020-11-25 06:54:14 +0300

    Define Liftable instance for U_T functor scheme

--- Pandora/Paradigm/Schemes/U_T.hs
+++ Pandora/Paradigm/Schemes/U_T.hs
@@ -1,8 +1,12 @@
 module Pandora.Paradigm.Schemes.U_T where
 
+import Pandora.Core.Functor (type (~>))
+import Pandora.Pattern.Category (($))
 import Pandora.Pattern.Functor.Covariant (Covariant)
 import Pandora.Pattern.Functor.Contravariant (Contravariant)
-import Pandora.Paradigm.Primary.Functor.Product (type (:*:))
+import Pandora.Pattern.Functor.Avoidable (Avoidable (empty))
+import Pandora.Pattern.Transformer.Liftable (Liftable (lift))
+import Pandora.Paradigm.Primary.Functor.Product (Product ((:*:)), type (:*:))
 import Pandora.Paradigm.Controlflow.Effect.Interpreted (Interpreted (Primary, run))
 
 newtype U_T ct cu t u a = U_T (u a :*: t a)
@@ -15,3 +19,7 @@ type (>.:.<) = U_T Contravariant Contravariant
 instance Interpreted (U_T ct cu t u) where
 	type Primary (U_T ct cu t u) a = u a :*: t a
 	run ~(U_T x) = x
+
+instance Avoidable t => Liftable (U_T Covariant Covariant t) where
+	lift :: Covariant u => u ~> t <.:.> u
+	lift x = U_T $ x :*: empty
