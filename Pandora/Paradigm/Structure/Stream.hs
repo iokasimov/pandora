4b1e4883da996a098211bff33813b1b918e24b6b
Murat Kasimov <iokasimov.m@gmail.com>
2020-11-30 07:00:57 +0300

    Add overlapping Extendable instance of Zipper Stream

--- Pandora/Paradigm/Structure/Stream.hs
+++ Pandora/Paradigm/Structure/Stream.hs
@@ -3,14 +3,16 @@
 module Pandora.Paradigm.Structure.Stream where
 
 import Pandora.Pattern.Category ((.), ($))
+import Pandora.Pattern.Functor.Covariant ((<$>))
 import Pandora.Pattern.Functor.Pointable (point)
 import Pandora.Pattern.Functor.Extractable (extract)
+import Pandora.Pattern.Functor.Extendable (Extendable ((=>>)))
 import Pandora.Paradigm.Primary.Functor.Delta (Delta ((:^:)))
 import Pandora.Paradigm.Primary.Functor.Identity (Identity (Identity))
 import Pandora.Paradigm.Primary.Functor.Wye (Wye (Left, Right))
-import Pandora.Paradigm.Primary.Transformer.Construction (Construction (Construct), deconstruct)
+import Pandora.Paradigm.Primary.Transformer.Construction (Construction (Construct), deconstruct, iterate)
 import Pandora.Paradigm.Primary.Transformer.Tap (Tap (Tap))
-import Pandora.Paradigm.Structure.Ability.Rotatable (Rotatable (Rotational, rotation))
+import Pandora.Paradigm.Structure.Ability.Rotatable (Rotatable (Rotational, rotation), rotate)
 import Pandora.Paradigm.Structure.Ability.Zipper (Zipper)
 import Pandora.Paradigm.Schemes.TU (TU (TU), type (<:.>))
 
@@ -27,6 +29,10 @@ instance Rotatable Right (Tap (Delta <:.> Stream)) where
 	type Rotational Right (Tap (Delta <:.> Stream)) a = Tap (Delta <:.> Stream) a
 	rotation (extract -> Tap x (TU (bs :^: fs))) = Tap (extract fs) . TU
 		$ Construct x (point bs) :^: extract (deconstruct fs)
+
+instance {-# OVERLAPS #-} Extendable (Tap (Delta <:.> Stream)) where
+	z =>> f = let move rtt = extract . deconstruct $ iterate (point . rtt) z
+		in f <$> Tap z (TU $ move (rotate @Left) :^: move (rotate @Right))
 
 repeat :: a -> Stream a
 repeat x = Construct x . Identity $ repeat x
