a23a7d6193a8459f288f4044edaa361f71dc989b
Murat Kasimov <iokasimov.m@gmail.com>
2020-12-20 14:32:20 +0300

    Define Traversable and Pointable instances for Comprehension

--- Pandora/Paradigm/Structure/Ability/Comprehension.hs
+++ Pandora/Paradigm/Structure/Ability/Comprehension.hs
@@ -3,11 +3,13 @@
 module Pandora.Paradigm.Structure.Ability.Comprehension where
 
 import Pandora.Core.Functor (type (:=))
+import Pandora.Core.Morphism ((%))
 import Pandora.Pattern.Category ((.), ($))
 import Pandora.Pattern.Functor.Covariant (Covariant ((<$>)))
 import Pandora.Pattern.Functor.Pointable (Pointable (point))
 import Pandora.Pattern.Functor.Applicative (Applicative ((<*>)))
-import Pandora.Pattern.Functor.Avoidable (Avoidable ())
+import Pandora.Pattern.Functor.Avoidable (Avoidable (empty))
+import Pandora.Pattern.Functor.Traversable (Traversable ((->>)))
 import Pandora.Pattern.Functor.Bindable (Bindable ((>>=)))
 import Pandora.Pattern.Object.Semigroup (Semigroup ((+)))
 import Pandora.Paradigm.Schemes.TU (TU (TU), type (<:.>))
@@ -22,6 +24,12 @@ instance Interpreted (Comprehension t) where
 
 instance Covariant (t <:.> Construction t) => Covariant (Comprehension t) where
 	f <$> Comprehension x = Comprehension $ f <$> x
+
+instance (Avoidable t, Pointable t) => Pointable (Comprehension t) where
+	point = Comprehension . TU . point . Construct % empty
+
+instance Traversable (t <:.> Construction t) => Traversable (Comprehension t) where
+	Comprehension x ->> f = Comprehension <$> x ->> f
 
 instance (forall a . Semigroup (t <:.> Construction t := a), Bindable t, Pointable t, Avoidable t) => Applicative (Comprehension t) where
 	fs <*> xs = fs >>= \f -> xs >>= Comprehension . TU . point . point . f
