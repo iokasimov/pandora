4a3a4a9ba54c5ef5d582adec2eafa3fc3afa0b96
Murat Kasimov <iokasimov.m@gmail.com>
2021-01-16 08:15:49 +0300

    Define Divisible instance for Equivalence

--- Pandora/Paradigm/Primary/Functor/Equivalence.hs
+++ Pandora/Paradigm/Primary/Functor/Equivalence.hs
@@ -2,9 +2,17 @@ module Pandora.Paradigm.Primary.Functor.Equivalence where
 
 import Pandora.Pattern.Category (($))
 import Pandora.Pattern.Functor.Contravariant (Contravariant ((>$<)))
+import Pandora.Pattern.Functor.Divisible (Divisible ((>*<)))
+import Pandora.Pattern.Object.Ringoid (Ringoid ((*)))
 import Pandora.Paradigm.Primary.Object.Boolean (Boolean)
+import Pandora.Paradigm.Primary.Functor.Product (Product ((:*:)))
 
 data Equivalence a = Equivalence (a -> a -> Boolean)
 
 instance Contravariant Equivalence where
 	f >$< Equivalence g = Equivalence $ \x y -> g (f x) (f y)
+
+instance Divisible Equivalence where
+	f >*< Equivalence g = \(Equivalence h) -> Equivalence $ \x y -> case f x of
+		(xl :*: xr) -> case f y of
+			(yl :*: yr) -> g xl yl * h xr yr
