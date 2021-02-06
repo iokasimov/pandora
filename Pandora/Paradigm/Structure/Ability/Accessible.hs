eceda2d73dab86eff80fb436873ea8cd3d5ea07d
Murat Kasimov <iokasimov.m@gmail.com>
2021-02-06 08:28:23 +0300

    Experimental transitive instance for Accessible

--- Pandora/Paradigm/Structure/Ability/Accessible.hs
+++ Pandora/Paradigm/Structure/Ability/Accessible.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module Pandora.Paradigm.Structure.Ability.Accessible where
 
-import Pandora.Paradigm.Inventory.Optics (type (:-.))
+import Pandora.Paradigm.Inventory.Optics (type (:-.), (|>))
 
 class Accessible tgt src where
 	access :: src :-. tgt
+
+instance (Accessible b a, Accessible c b) => Accessible c a where
+	access = access @b @a |> access @c @b
