f0856994b92bfeaee7d4b64f5d4bffc749a45e95
Murat Kasimov <iokasimov.m@gmail.com>
2021-02-06 10:52:14 +0300

    Redefine Zipper for Stream with T_U joint scheme

--- Pandora/Paradigm/Structure/Stream.hs
+++ Pandora/Paradigm/Structure/Stream.hs
@@ -4,37 +4,37 @@ module Pandora.Paradigm.Structure.Stream where
 
 import Pandora.Core.Functor (type (:=>))
 import Pandora.Pattern.Category ((.), ($))
-import Pandora.Pattern.Functor.Covariant ((<$>))
+import Pandora.Pattern.Functor.Covariant (Covariant ((<$>)))
 import Pandora.Pattern.Functor.Pointable (point)
 import Pandora.Pattern.Functor.Extractable (extract)
 import Pandora.Pattern.Functor.Extendable (Extendable ((=>>)))
 import Pandora.Paradigm.Controlflow.Effect.Interpreted (run)
-import Pandora.Paradigm.Primary.Functor.Delta (Delta ((:^:)))
+import Pandora.Paradigm.Primary.Functor.Product (Product ((:*:)), type (:*:))
 import Pandora.Paradigm.Primary.Functor.Identity (Identity (Identity))
 import Pandora.Paradigm.Primary.Functor.Wye (Wye (Left, Right))
 import Pandora.Paradigm.Primary.Transformer.Construction (Construction (Construct), deconstruct, (.-+))
 import Pandora.Paradigm.Primary.Transformer.Tap (Tap (Tap))
 import Pandora.Paradigm.Structure.Ability.Morphable (Morphable (Morphing, morphing), morph)
 import Pandora.Paradigm.Structure.Ability.Zipper (Zipper)
-import Pandora.Paradigm.Schemes.TU (TU (TU), type (<:.>))
+import Pandora.Paradigm.Schemes.T_U (T_U (T_U), type (<:.:>))
 
 type Stream = Construction Identity
 
-type instance Zipper Stream = Tap (Delta <:.> Stream)
+type instance Zipper Stream = Tap ((:*:) <:.:> Stream)
 
-instance Morphable Left (Tap (Delta <:.> Stream)) where
-	type Morphing Left (Tap (Delta <:.> Stream)) = Tap (Delta <:.> Stream)
-	morphing (extract . run -> Tap x (TU (bs :^: fs))) = Tap (extract bs) . TU
-		$ extract (deconstruct bs) :^: Construct x (point fs)
+instance Morphable Left (Tap ((:*:) <:.:> Stream)) where
+	type Morphing Left (Tap ((:*:) <:.:> Stream)) = Tap ((:*:) <:.:> Stream)
+	morphing (extract . run -> Tap x (T_U (bs :*: fs))) = Tap (extract bs) . T_U
+		$ extract (deconstruct bs) :*: Construct x (point fs)
 
-instance Morphable Right (Tap (Delta <:.> Stream)) where
-	type Morphing Right (Tap (Delta <:.> Stream)) = Tap (Delta <:.> Stream)
-	morphing (extract . run -> Tap x (TU (bs :^: fs))) = Tap (extract fs) . TU
-		$ Construct x (point bs) :^: extract (deconstruct fs)
+instance Morphable Right (Tap ((:*:) <:.:> Stream)) where
+	type Morphing Right (Tap ((:*:) <:.:> Stream)) = Tap ((:*:) <:.:> Stream)
+	morphing (extract . run -> Tap x (T_U (bs :*: fs))) = Tap (extract fs) . T_U
+		$ Construct x (point bs) :*: extract (deconstruct fs)
 
-instance {-# OVERLAPS #-} Extendable (Tap (Delta <:.> Stream)) where
+instance {-# OVERLAPS #-} Extendable (Tap ((:*:) <:.:> Stream)) where
 	z =>> f = let move rtt = extract . deconstruct $ point . rtt .-+ z
-		in f <$> Tap z (TU $ move (morph @Left) :^: move (morph @Right))
+		in f <$> Tap z (T_U $ move (morph @Left) :*: move (morph @Right))
 
 repeat :: a :=> Stream
 repeat x = Construct x . Identity $ repeat x
