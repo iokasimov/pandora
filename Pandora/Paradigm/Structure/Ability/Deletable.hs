28f0d626e55cc9c5b58dffdaa64ff8a33a014615
Murat Kasimov <iokasimov.m@gmail.com>
2021-02-08 20:05:52 +0300

    Define fixity and precedence for += and -= methods

--- Pandora/Paradigm/Structure/Ability/Deletable.hs
+++ Pandora/Paradigm/Structure/Ability/Deletable.hs
@@ -2,5 +2,7 @@ module Pandora.Paradigm.Structure.Ability.Deletable where
 
 import Pandora.Pattern.Object.Setoid (Setoid)
 
+infixr 3 -=
+
 class Deletable t where
 	(-=) :: Setoid a => a -> t a -> t a
--- Pandora/Paradigm/Structure/Ability/Insertable.hs
+++ Pandora/Paradigm/Structure/Ability/Insertable.hs
@@ -1,4 +1,6 @@
 module Pandora.Paradigm.Structure.Ability.Insertable where
 
+infixr 2 +=
+
 class Insertable t where
 	(+=) :: a -> t a -> t a
