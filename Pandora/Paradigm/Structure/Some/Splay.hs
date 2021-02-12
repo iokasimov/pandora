922c73d75940eb3fa38da1c30e41cc3ff7a752f3
Murat Kasimov <iokasimov.m@gmail.com>
2021-02-12 14:40:31 +0300

    Add Rotate verb for Morphable instance to rotate Splay trees

--- Pandora/Paradigm/Structure/Some/Splay.hs
+++ Pandora/Paradigm/Structure/Some/Splay.hs
@@ -14,38 +14,38 @@ import Pandora.Paradigm.Primary.Functor.Wye (Wye (Left, Right))
 import Pandora.Paradigm.Primary.Transformer.Construction (Construction (Construct), deconstruct)
 import Pandora.Paradigm.Controlflow.Effect.Interpreted (run, (||=))
 import Pandora.Paradigm.Schemes (TU (TU))
-import Pandora.Paradigm.Structure.Ability.Morphable (Morphable (Morphing, morphing), morph)
+import Pandora.Paradigm.Structure.Ability.Morphable (Morphable (Morphing, morphing), Morph (Rotate), morph)
 import Pandora.Paradigm.Structure.Ability.Substructure (substitute)
 import Pandora.Paradigm.Structure.Some.Binary (Binary)
 
 data Splay a = Zig a | Zag a
 
-instance Morphable (Left Zig) (Construction Wye) where
-	type Morphing (Left Zig) (Construction Wye) = Binary
+instance Morphable (Rotate (Left Zig)) (Construction Wye) where
+	type Morphing (Rotate (Left Zig)) (Construction Wye) = Binary
 	morphing (extract . run -> Construct parent st) = TU $ Construct % subtree . extract <$> left st where
 
 		subtree = branches (deconstruct <$> left st >>= left) . Just . Construct parent
 			$ branches (deconstruct <$> left st >>= right) (right st)
 
-instance Morphable (Right Zig) (Construction Wye) where
-	type Morphing (Right Zig) (Construction Wye) = Binary
+instance Morphable (Rotate (Right Zig)) (Construction Wye) where
+	type Morphing (Rotate (Right Zig)) (Construction Wye) = Binary
 	morphing (extract . run -> Construct parent st) = TU $ Construct % subtree . extract <$> right st where
 
 		subtree = branches (left st) . Just . Construct parent
 			$ branches (deconstruct <$> right st >>= left) (deconstruct <$> right st >>= right)
 
-instance Morphable (Left (Zig Zig)) (Construction Wye) where
-	type Morphing (Left (Zig Zig)) (Construction Wye) = Binary
-	morphing (extract . run -> tree) = TU $ run (morph @(Left Zig) tree) >>= run . morph @(Left Zig)
+instance Morphable (Rotate (Left (Zig Zig))) (Construction Wye) where
+	type Morphing (Rotate (Left (Zig Zig))) (Construction Wye) = Binary
+	morphing (extract . run -> tree) = TU $ run (morph @(Rotate (Left Zig)) tree) >>= run . morph @(Rotate (Left Zig))
 
-instance Morphable (Right (Zig Zig)) (Construction Wye) where
-	type Morphing (Right (Zig Zig)) (Construction Wye) = Binary
-	morphing (extract . run -> tree) = TU $ run (morph @(Right Zig) tree) >>= run . morph @(Right Zig)
+instance Morphable (Rotate (Right (Zig Zig))) (Construction Wye) where
+	type Morphing (Rotate (Right (Zig Zig))) (Construction Wye) = Binary
+	morphing (extract . run -> tree) = TU $ run (morph @(Rotate (Right Zig)) tree) >>= run . morph @(Rotate (Right Zig))
 
-instance Morphable (Left (Zig Zag)) (Construction Wye) where
-	type Morphing (Left (Zig Zag)) (Construction Wye) = Binary
-	morphing = morph @(Left Zig) . substitute @Left ((>>= run . morph @(Right Zig)) ||=) . extract . run
+instance Morphable (Rotate (Left (Zig Zag))) (Construction Wye) where
+	type Morphing (Rotate (Left (Zig Zag))) (Construction Wye) = Binary
+	morphing = morph @(Rotate (Left Zig)) . substitute @Left ((>>= run . morph @(Rotate (Right Zig))) ||=) . extract . run
 
-instance Morphable (Right (Zig Zag)) (Construction Wye) where
-	type Morphing (Right (Zig Zag)) (Construction Wye) = Binary
-	morphing = morph @(Right Zig) . substitute @Right ((>>= run . morph @(Left Zig)) ||=) . extract . run
+instance Morphable (Rotate (Right (Zig Zag))) (Construction Wye) where
+	type Morphing (Rotate (Right (Zig Zag))) (Construction Wye) = Binary
+	morphing = morph @(Rotate (Right Zig)) . substitute @Right ((>>= run . morph @(Rotate (Left Zig))) ||=) . extract . run
