b85c9fed14885723fac65f320ecceb91883abd63
Murat Kasimov <iokasimov.m@gmail.com>
2021-02-13 16:47:33 +0300

    Define empty module of Dictionary interface

--- Pandora/Paradigm/Structure/Interface.hs
+++ Pandora/Paradigm/Structure/Interface.hs
@@ -1,3 +1,4 @@
 module Pandora.Paradigm.Structure.Interface (module Exports) where
 
+import Pandora.Paradigm.Structure.Interface.Dictionary as Exports
 import Pandora.Paradigm.Structure.Interface.Set as Exports
---
+++ Pandora/Paradigm/Structure/Interface/Dictionary.hs
@@ -0,0 +1,1 @@
+module Pandora.Paradigm.Structure.Interface.Dictionary where
