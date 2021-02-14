f02f371a3c06aa21abaa5a73220c60e8417764e4
Murat Kasimov <iokasimov.m@gmail.com>
2021-02-14 18:17:54 +0300

    Define Pointable instance for Prefixed

--- Pandora/Paradigm/Structure/Modification/Prefixed.hs
+++ Pandora/Paradigm/Structure/Modification/Prefixed.hs
@@ -1,12 +1,14 @@
 module Pandora.Paradigm.Structure.Modification.Prefixed where
 
 import Pandora.Core.Functor (type (:=))
-import Pandora.Pattern.Category (($))
+import Pandora.Pattern.Category ((.), ($))
 import Pandora.Pattern.Functor.Covariant (Covariant ((<$>)))
+import Pandora.Pattern.Functor.Pointable (Pointable (point))
 import Pandora.Pattern.Functor.Traversable (Traversable ((->>)))
-import Pandora.Paradigm.Primary.Functor.Maybe (Maybe)
-import Pandora.Paradigm.Primary.Functor.Product (Product)
-import Pandora.Paradigm.Schemes.TU (type (<:.>))
+import Pandora.Pattern.Object.Monoid (Monoid (zero))
+import Pandora.Paradigm.Primary.Functor.Maybe (Maybe (Just))
+import Pandora.Paradigm.Primary.Functor.Product (Product ((:*:)))
+import Pandora.Paradigm.Schemes.TU (TU (TU), type (<:.>))
 
 newtype Prefixed t k a = Prefixed (t <:.> (Product k <:.> Maybe) := a)
 
@@ -15,3 +17,6 @@ instance Covariant t => Covariant (Prefixed t k) where
 
 instance Traversable t => Traversable (Prefixed t k) where
 	Prefixed x ->> f = Prefixed <$> (x ->> f)
+
+instance (Monoid k, Pointable t) => Pointable (Prefixed t k) where
+	point = Prefixed . TU . point . TU . (:*:) zero . Just
