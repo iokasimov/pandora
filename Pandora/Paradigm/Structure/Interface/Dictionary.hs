b9d26b023d90dbe4fb4f90f7e1a4eb63534615eb
Murat Kasimov <iokasimov.m@gmail.com>
2021-02-16 13:10:36 +0300

    Define Interpreted instance for Prefixed

--- Pandora/Paradigm/Structure/Interface/Dictionary.hs
+++ Pandora/Paradigm/Structure/Interface/Dictionary.hs
@@ -1,6 +1,5 @@
 module Pandora.Paradigm.Structure.Interface.Dictionary where
 
-import Pandora.Core.Functor (type (~>))
 import Pandora.Paradigm.Primary.Functor.Maybe (Maybe)
 
 class Dictionary a k t where
--- Pandora/Paradigm/Structure/Modification/Prefixed.hs
+++ Pandora/Paradigm/Structure/Modification/Prefixed.hs
@@ -5,16 +5,18 @@ module Pandora.Paradigm.Structure.Modification.Prefixed where
 import Pandora.Core.Functor (type (:=))
 import Pandora.Pattern.Category ((.), ($))
 import Pandora.Pattern.Functor.Covariant (Covariant ((<$>)))
+import Pandora.Pattern.Functor.Contravariant ((>$<))
 import Pandora.Pattern.Functor.Pointable (Pointable (point))
 import Pandora.Pattern.Functor.Extractable (Extractable (extract))
 import Pandora.Pattern.Functor.Traversable (Traversable ((->>)))
 import Pandora.Pattern.Functor.Bindable (Bindable ((>>=)))
+import Pandora.Pattern.Transformer.Liftable (lift)
 import Pandora.Pattern.Object.Monoid (Monoid (zero))
-import Pandora.Pattern.Object.Setoid (Setoid ((==)))
+import Pandora.Pattern.Object.Setoid (Setoid)
 import Pandora.Paradigm.Primary.Functor.Maybe (Maybe (Just))
 import Pandora.Paradigm.Primary.Functor.Product (Product ((:*:)), attached)
-import Pandora.Paradigm.Primary.Functor.Predicate (Predicate (Predicate))
-import Pandora.Paradigm.Controlflow.Effect.Interpreted (run)
+import Pandora.Paradigm.Primary.Functor.Predicate (equate)
+import Pandora.Paradigm.Controlflow.Effect.Interpreted (Interpreted (Primary, run, unite))
 import Pandora.Paradigm.Schemes.TU (TU (TU), type (<:.>))
 import Pandora.Paradigm.Structure.Ability.Monotonic (Monotonic, find)
 import Pandora.Paradigm.Structure.Interface.Dictionary (Dictionary ((?=)))
@@ -23,6 +25,11 @@ type Keyed k = Product k <:.> Maybe
 
 newtype Prefixed t k a = Prefixed (t <:.> Keyed k := a)
 
+instance Interpreted (Prefixed t k) where
+	type Primary (Prefixed t k) a = t <:.> Keyed k := a
+	run ~(Prefixed x) = x
+	unite = Prefixed
+
 instance Covariant t => Covariant (Prefixed t k) where
 	f <$> Prefixed x = Prefixed $ f <$> x
 
@@ -30,7 +37,7 @@ instance Traversable t => Traversable (Prefixed t k) where
 	Prefixed x ->> f = Prefixed <$> (x ->> f)
 
 instance (Monoid k, Pointable t) => Pointable (Prefixed t k) where
-	point = Prefixed . TU . point . TU . (:*:) zero . Just
+	point = Prefixed . lift . TU . (:*:) zero . Just
 
 instance (Monotonic (Keyed k a) (t (Keyed k a)), Setoid k) => Dictionary a k (Prefixed t k) where
-	k ?= Prefixed x = find @(Keyed k a) @(t (Keyed k a)) (Predicate $ (== k) . attached . run) (run x) >>= extract . run
+	k ?= Prefixed x = find @(Keyed k a) (attached . run >$< equate k) (run x) >>= extract . run
