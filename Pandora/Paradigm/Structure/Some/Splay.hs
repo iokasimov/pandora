a97709db9afd148af7e58a27d7e6c93722733467
Murat Kasimov <iokasimov.m@gmail.com>
2021-02-19 15:25:01 +0300

    Fix Morphable instances for Splay tree: Left and Right zigs

--- Pandora/Paradigm/Structure/Some/Splay.hs
+++ Pandora/Paradigm/Structure/Some/Splay.hs
@@ -1,20 +1,25 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 
 module Pandora.Paradigm.Structure.Some.Splay where
 
+import Pandora.Core.Functor (type (~>), type (:.), type (:=))
 import Pandora.Pattern.Category ((.), ($))
 import Pandora.Pattern.Functor.Covariant (Covariant ((<$>)))
+import Pandora.Pattern.Functor.Applicative ((<*>))
 import Pandora.Pattern.Functor.Extractable (extract)
 import Pandora.Pattern.Functor.Bindable (Bindable ((>>=)))
 import Pandora.Paradigm.Primary ()
 import Pandora.Paradigm.Primary.Functor (branches)
 import Pandora.Paradigm.Primary.Functor.Function ((%))
 import Pandora.Paradigm.Primary.Functor.Maybe (Maybe (Just))
+import Pandora.Paradigm.Primary.Functor.Tagged (type (:#))
 import Pandora.Paradigm.Primary.Functor.Wye (Wye (Left, Right))
 import Pandora.Paradigm.Primary.Transformer.Construction (Construction (Construct), deconstruct)
 import Pandora.Paradigm.Controlflow.Effect.Interpreted (run, (||=))
-import Pandora.Paradigm.Schemes (TU (TU))
-import Pandora.Paradigm.Structure.Ability.Morphable (Morphable (Morphing, morphing), Morph (Rotate), rotate, into)
+import Pandora.Paradigm.Schemes (TU (TU), type (<:.>))
+import Pandora.Paradigm.Structure.Ability.Morphable (Morphable (Morphing, morphing), Morph (Rotate, Into), rotate, into)
+import Pandora.Paradigm.Structure.Ability.Nonempty (Nonempty)
 import Pandora.Paradigm.Structure.Ability.Substructure (substitute)
 import Pandora.Paradigm.Structure.Some.Binary (Binary)
 
@@ -22,18 +27,28 @@ data Splay a = Zig a | Zag a
 
 instance Morphable (Rotate (Left Zig)) (Construction Wye) where
 	type Morphing (Rotate (Left Zig)) (Construction Wye) = Binary
-	morphing (extract . run -> Construct parent st) = TU $ Construct % subtree . extract <$> into @(Left Maybe) st where
+	morphing :: forall a . (:#) (Rotate (Left Zig)) <:.> Construction Wye := a -> Binary a
+	morphing (extract . run -> Construct x xs) = TU $ Construct <$> parent <*> Just nodes where
 
-		subtree = branches (deconstruct <$> into @(Left Maybe) st >>= into @(Left Maybe)) . Just . Construct parent
-			$ branches (deconstruct <$> into @(Left Maybe) st >>= into @(Right Maybe)) (into @(Right Maybe) st)
+		nodes :: Wye :. Nonempty Binary := a
+		nodes = branches (branch @Left xs) . Just . Construct x
+			$ branches (deconstruct <$> branch @Right xs >>= branch @Left)
+				(deconstruct <$> branch @Right xs >>= branch @Right)
+
+		parent :: Maybe a
+		parent = extract <$> branch @Right xs
 
 instance Morphable (Rotate (Right Zig)) (Construction Wye) where
 	type Morphing (Rotate (Right Zig)) (Construction Wye) = Binary
-	morphing (extract . run -> Construct parent st) = TU $ Construct % subtree . extract <$> into @(Right Maybe) st where
+	morphing :: forall a . (:#) (Rotate (Right Zig)) <:.> Construction Wye := a -> Binary a
+	morphing (extract . run -> Construct x xs) = TU $ Construct <$> parent <*> Just nodes where
 
-		subtree = branches (into @(Left Maybe) st) . Just . Construct parent
-			$ branches (deconstruct <$> into @(Right Maybe) st >>= into @(Left Maybe))
-				(deconstruct <$> into @(Right Maybe) st >>= into @(Right Maybe))
+		nodes :: Wye :. Nonempty Binary := a
+		nodes = branches (deconstruct <$> branch @Left xs >>= branch @Left) . Just . Construct x
+			$ branches (deconstruct <$> branch @Left xs >>= branch @Right) (branch @Right xs)
+
+		parent :: Maybe a
+		parent = extract <$> branch @Left xs
 
 instance Morphable (Rotate (Left (Zig Zig))) (Construction Wye) where
 	type Morphing (Rotate (Left (Zig Zig))) (Construction Wye) = Binary
@@ -50,3 +65,6 @@ instance Morphable (Rotate (Left (Zig Zag))) (Construction Wye) where
 instance Morphable (Rotate (Right (Zig Zag))) (Construction Wye) where
 	type Morphing (Rotate (Right (Zig Zag))) (Construction Wye) = Binary
 	morphing = rotate @(Right Zig) . substitute @Right ((>>= run . rotate @(Left Zig)) ||=) . extract . run
+
+branch :: forall b . Morphable (Into (b Maybe)) Wye => Wye ~> Morphing (Into (b Maybe)) Wye
+branch = into @(b Maybe)
