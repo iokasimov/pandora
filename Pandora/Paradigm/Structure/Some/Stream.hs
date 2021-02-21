c00bcbf5a922a944f2837dccdd875a0f6e89b1e4
Murat Kasimov <iokasimov.m@gmail.com>
2021-02-21 10:21:52 +0300

    Use twosome in Stream module instead of T_U

--- Pandora/Paradigm/Structure/Some/Stream.hs
+++ Pandora/Paradigm/Structure/Some/Stream.hs
@@ -9,7 +9,7 @@ import Pandora.Pattern.Functor.Pointable (point)
 import Pandora.Pattern.Functor.Extractable (extract)
 import Pandora.Pattern.Functor.Extendable (Extendable ((=>>)))
 import Pandora.Paradigm.Controlflow.Effect.Interpreted (run)
-import Pandora.Paradigm.Primary.Functor.Product (Product ((:*:)), type (:*:))
+import Pandora.Paradigm.Primary.Functor.Product (Product ((:*:)), type (:*:), twosome)
 import Pandora.Paradigm.Primary.Functor.Identity (Identity (Identity))
 import Pandora.Paradigm.Primary.Functor.Wye (Wye (Left, Right))
 import Pandora.Paradigm.Primary.Transformer.Construction (Construction (Construct), deconstruct, (.-+))
@@ -24,17 +24,17 @@ type instance Zipper Stream = Tap ((:*:) <:.:> Stream)
 
 instance Morphable (Rotate Left) (Tap ((:*:) <:.:> Stream)) where
 	type Morphing (Rotate Left) (Tap ((:*:) <:.:> Stream)) = Tap ((:*:) <:.:> Stream)
-	morphing (extract . run -> Tap x (T_U (bs :*: fs))) = Tap (extract bs) . T_U
-		$ extract (deconstruct bs) :*: Construct x (point fs)
+	morphing (extract . run -> Tap x (T_U (bs :*: fs))) = Tap (extract bs)
+		$ twosome (extract $ deconstruct bs) (Construct x $ point fs)
 
 instance Morphable (Rotate Right) (Tap ((:*:) <:.:> Stream)) where
 	type Morphing (Rotate Right) (Tap ((:*:) <:.:> Stream)) = Tap ((:*:) <:.:> Stream)
-	morphing (extract . run -> Tap x (T_U (bs :*: fs))) = Tap (extract fs) . T_U
-		$ Construct x (point bs) :*: extract (deconstruct fs)
+	morphing (extract . run -> Tap x (T_U (bs :*: fs))) = Tap (extract fs)
+		$ twosome (Construct x $ point bs) (extract $ deconstruct fs)
 
 instance {-# OVERLAPS #-} Extendable (Tap ((:*:) <:.:> Stream)) where
 	z =>> f = let move rtt = extract . deconstruct $ point . rtt .-+ z
-		in f <$> Tap z (T_U $ move (rotate @Left) :*: move (rotate @Right))
+		in f <$> Tap z (twosome (move $ rotate @Left) (move $ rotate @Right))
 
 repeat :: a :=> Stream
 repeat x = Construct x . Identity $ repeat x
