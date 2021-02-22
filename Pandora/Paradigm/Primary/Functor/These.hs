6e0b40678655305646c53fce6a116acc5a674aeb
Murat Kasimov <iokasimov.m@gmail.com>
2021-02-22 17:28:09 +0300

    Define Semigroup instance for These

--- Pandora/Paradigm/Primary/Functor/These.hs
+++ Pandora/Paradigm/Primary/Functor/These.hs
@@ -4,6 +4,7 @@ import Pandora.Pattern.Category (($))
 import Pandora.Pattern.Functor.Covariant (Covariant ((<$>)))
 import Pandora.Pattern.Functor.Pointable (Pointable (point))
 import Pandora.Pattern.Functor.Traversable (Traversable ((->>)))
+import Pandora.Pattern.Object.Semigroup (Semigroup ((+)))
 
 data These e a = This a | That e | These e a
 
@@ -19,6 +20,17 @@ instance Traversable (These e) where
 	This x ->> f = This <$> f x
 	That y ->> _ = point $ That y
 	These y x ->> f = These y <$> f x
+
+instance (Semigroup e, Semigroup a) => Semigroup (These e a) where
+	This x + This x' = This (x + x')
+	This x + That y = These y x
+	This x + These y x' = These y (x + x')
+	That y + This x' = These y x'
+	That y + That y' = That (y + y')
+	That y + These y' x = These (y + y') x
+	These y x + This x' = These y (x + x')
+	These y x + That y' = These (y + y') x
+	These y x + These y' x' = These (y + y') (x + x')
 
 these :: (a -> r) -> (e -> r) -> (e -> a -> r) -> These e a -> r
 these f _ _ (This x) = f x
