4fd8a9a9b73fdd684dfd87116a2458586ce1c66a
Murat Kasimov <iokasimov.m@gmail.com>
2021-02-24 17:50:53 +0300

    Define Interpreted instance for Continuation

--- Pandora/Paradigm/Primary/Transformer/Continuation.hs
+++ Pandora/Paradigm/Primary/Transformer/Continuation.hs
@@ -9,9 +9,15 @@ import Pandora.Pattern.Functor.Traversable (Traversable)
 import Pandora.Pattern.Functor.Bindable (Bindable ((>>=)))
 import Pandora.Pattern.Functor.Monad (Monad)
 import Pandora.Pattern.Transformer.Liftable (Liftable (lift))
+import Pandora.Paradigm.Controlflow.Effect.Interpreted (Interpreted (Primary, run, unite))
 import Pandora.Paradigm.Primary.Functor.Function ((!), (%))
 
-newtype Continuation r t a = Continuation { continue :: (->) ::|:. a :. t := r }
+newtype Continuation r t a = Continuation ((->) ::|:. a :. t := r)
+
+instance Interpreted (Continuation r t) where
+	type Primary (Continuation r t) a = (->) ::|:. a :. t := r
+	run ~(Continuation x) = x
+	unite = Continuation
 
 instance Covariant t => Covariant (Continuation r t) where
 	f <$> Continuation continuation = Continuation $ continuation . (. f)
@@ -20,10 +26,10 @@ instance Covariant t => Pointable (Continuation r t) where
 	point x = Continuation ($ x)
 
 instance Covariant t => Applicative (Continuation r t) where
-	f <*> x = Continuation $ \h -> continue f $ \g -> continue x (h . g)
+	f <*> x = Continuation $ \h -> run f $ \g -> run x (h . g)
 
 instance Covariant t => Bindable (Continuation r t) where
-	x >>= f = Continuation $ \g -> continue x $ \y -> continue (f y) g
+	x >>= f = Continuation $ \g -> run x $ \y -> run (f y) g
 
 instance Monad t => Monad (Continuation r t) where
 
@@ -32,15 +38,15 @@ instance (forall u . Bindable u) => Liftable (Continuation r) where
 
 -- | Call with current continuation
 cwcc :: ((a -> Continuation r t b) -> Continuation r t a) -> Continuation r t a
-cwcc f = Continuation $ \g -> continue % g . f $ Continuation . (!) . g
+cwcc f = Continuation $ \g -> run % g . f $ Continuation . (!) . g
 
 -- | Delimit the continuation of any 'shift'
 reset :: (forall u . Bindable u, Monad t, Traversable t) => Continuation r t r -> Continuation s t r
-reset = lift . continue % point
+reset = lift . run % point
 
 -- | Capture the continuation up to the nearest enclosing 'reset' and pass it
 shift :: Pointable t => ((a -> t r) -> Continuation r t r) -> Continuation r t a
-shift f = Continuation $ continue % point . f
+shift f = Continuation $ run % point . f
 
 interruptable :: Pointable t => ((a -> Continuation a t a) -> Continuation a t a) -> t a
-interruptable = continue % point . cwcc
+interruptable = run % point . cwcc
