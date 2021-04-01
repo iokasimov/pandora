8dd2ff5537a830d968204d551a01793e62fdd353
Murat Kasimov <iokasimov.m@gmail.com>
2021-04-01 18:33:20 +0300

    Redefine subset method of Set interface

--- Pandora/Paradigm/Structure/Interface/Set.hs
+++ Pandora/Paradigm/Structure/Interface/Set.hs
@@ -1,24 +1,31 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+
 module Pandora.Paradigm.Structure.Interface.Set where
 
+import Pandora.Core.Functor (type (:=))
 import Pandora.Pattern.Category ((.), ($))
 import Pandora.Pattern.Functor.Traversable (Traversable ((->>)))
-import Pandora.Pattern.Object.Setoid (Setoid)
+import Pandora.Pattern.Object.Setoid (Setoid ((!=)))
 import Pandora.Pattern.Object.Semigroup ((+))
 import Pandora.Pattern.Object.Quasiring (one)
 import Pandora.Paradigm.Primary.Functor.Function ((!), (%))
+import Pandora.Paradigm.Primary.Functor.Maybe (Maybe (Nothing))
+import Pandora.Paradigm.Primary.Functor.Predicate (Predicate, equate)
 import Pandora.Paradigm.Primary.Functor.Product (attached)
+import Pandora.Paradigm.Primary.Object.Boolean (Boolean)
 import Pandora.Paradigm.Primary.Object.Numerator (Numerator (Zero))
-import Pandora.Paradigm.Structure.Ability.Morphable (Morphable, Morph (Find, Element))
+import Pandora.Paradigm.Schemes.T_U (type (<:.:>))
+import Pandora.Paradigm.Structure.Ability.Morphable (Morphable (Morphing), Morph (Find), find)
 import Pandora.Paradigm.Inventory.State (State, modify)
 import Pandora.Paradigm.Controlflow.Effect (run)
 
-type Set t a = (Traversable t, Setoid a, Setoid (t a), Morphable (Find Element) t)
+type Set t f a = (Traversable t, Setoid a, Setoid (t a), Morphable (Find f) t)
 
 -- member :: forall e a . (Setoid a, Monotonic a e) => a -> e -> Boolean
 -- member x = reduce @a @(Maybe a) (True !!) False . find (equate x)
 
--- subset :: (Monotonic a (t a), Traversable t, Setoid a, Setoid (t a)) => t a -> t a -> Boolean
--- subset ss s = Nothing != ss ->> find % s . equate
+subset :: forall t f a . (Set t f a, Morphing (Find f) t ~ (Predicate <:.:> Maybe := (->))) => t a -> t a -> Boolean
+subset ss s = Nothing != ss ->> find @f @t @Maybe % s . equate
 
 cardinality :: Traversable t => t a -> Numerator
 cardinality s = attached . run @(State _) % Zero $ s ->> (modify @Numerator (+ one) !)
