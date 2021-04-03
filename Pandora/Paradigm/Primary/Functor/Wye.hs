2af9dbb4435a09b80162b81ae143a1ef100690fd
Murat Kasimov <iokasimov.m@gmail.com>
2021-04-03 14:00:59 +0300

    Define Semigroup and Monoid instances for Wye datatype

--- Pandora/Paradigm/Primary/Functor/Wye.hs
+++ Pandora/Paradigm/Primary/Functor/Wye.hs
@@ -6,6 +6,8 @@ import Pandora.Pattern.Functor.Covariant (Covariant ((<$>)))
 import Pandora.Pattern.Functor.Pointable (Pointable (point))
 import Pandora.Pattern.Functor.Applicative (Applicative ((<*>)))
 import Pandora.Pattern.Functor.Traversable (Traversable ((->>)))
+import Pandora.Pattern.Object.Semigroup (Semigroup ((+)))
+import Pandora.Pattern.Object.Monoid (Monoid (zero))
 import Pandora.Paradigm.Structure.Ability.Monotonic (Monotonic (reduce))
 
 data Wye a = End | Left a | Right a | Both a a
@@ -27,6 +29,22 @@ instance Monotonic a (Wye a) where
 	reduce f r (Right x) = f x r
 	reduce f r (Both x y) = f y (f x r)
 	reduce _ r End = r
+
+instance Semigroup a => Semigroup (Wye a) where
+	End + x = x
+	x + End = x
+	Left x + Left x' = Left / x + x'
+	Left x + Right y = Both x y
+	Left x + Both x' y = Both / x + x' / y
+	Right y + Left x = Both x y
+	Right y + Right y' = Right / y + y'
+	Right y + Both x y' = Both x / y + y'
+	Both x y + Left x' = Both / x + x' / y
+	Both x y + Right y' = Both / x / y + y'
+	Both x y + Both x' y' = Both / x + x' / y + y'
+
+instance Semigroup a => Monoid (Wye a) where
+	zero = End
 
 wye :: r -> (a -> r) -> (a -> r) -> (a -> a -> r) -> Wye a -> r
 wye r _ _ _ End = r
