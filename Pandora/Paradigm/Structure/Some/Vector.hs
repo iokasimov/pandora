1bbe3ce0670ef2d5cc7aef482d2ddc5cef68067e
Murat Kasimov <iokasimov.m@gmail.com>
2021-04-07 19:43:43 +0300

    Redefine Vector datastructure - make it a tuple

--- Pandora/Paradigm/Structure/Some/Vector.hs
+++ Pandora/Paradigm/Structure/Some/Vector.hs
@@ -1,5 +1,6 @@
 module Pandora.Paradigm.Structure.Some.Vector where
 
+import Pandora.Paradigm.Primary.Functor.Product (type (:*:))
 import Pandora.Paradigm.Structure.Ability.Monotonic (Monotonic)
 
-data Vector a = forall v . Monotonic a v => Vector v
+data Vector a = forall r . (Monotonic a (a :*: r), Monotonic a r) => Vector (a :*: r)
