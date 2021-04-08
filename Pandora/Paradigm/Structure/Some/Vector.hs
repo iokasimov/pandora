70236537961b5d491593bbe10aa752fbd5c80f5f
Murat Kasimov <iokasimov.m@gmail.com>
2021-04-08 19:45:33 +0300

    Redefine Vector datatype - make it GADT

--- Pandora/Paradigm/Structure/Some/Vector.hs
+++ Pandora/Paradigm/Structure/Some/Vector.hs
@@ -1,25 +1,7 @@
 module Pandora.Paradigm.Structure.Some.Vector where
 
-import Pandora.Pattern.Category (($))
-import Pandora.Pattern.Object.Semigroup (Semigroup ((+)))
-import Pandora.Pattern.Object.Ringoid (Ringoid ((*)))
-import Pandora.Pattern.Object.Monoid (Monoid (zero))
-import Pandora.Pattern.Object.Quasiring (Quasiring (one))
-import Pandora.Paradigm.Structure.Ability.Monotonic (Monotonic (reduce))
+import Pandora.Paradigm.Primary.Functor.Product (type (:*:))
 
-newtype Vector r a = Vector r
-
-instance Monotonic a r => Monotonic a (Vector r a) where
-	reduce f r (Vector x) = reduce f r x
-
-instance Semigroup r => Semigroup (Vector r a) where
-	Vector x + Vector y = Vector $ x + y
-
-instance Ringoid r => Ringoid (Vector r a) where
-	Vector x * Vector y = Vector $ x * y
-
-instance Monoid r => Monoid (Vector r a) where
-	zero = Vector zero
-
-instance Quasiring r => Quasiring (Vector r a) where
-	one = Vector one
+data Vector r a where
+	Scalar :: a -> Vector a a
+	Vector :: a -> Vector r a -> Vector (a :*: r) a
