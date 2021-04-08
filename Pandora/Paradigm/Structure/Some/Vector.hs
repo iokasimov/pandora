fc54cac6bad39ad11c3ad568d6e0d79c55221086
Murat Kasimov <iokasimov.m@gmail.com>
2021-04-08 18:56:36 +0300

    Define Ringoid, Monoid and Quasiring instances for Vector

--- Pandora/Paradigm/Structure/Some/Vector.hs
+++ Pandora/Paradigm/Structure/Some/Vector.hs
@@ -2,6 +2,9 @@ module Pandora.Paradigm.Structure.Some.Vector where
 
 import Pandora.Pattern.Category (($))
 import Pandora.Pattern.Object.Semigroup (Semigroup ((+)))
+import Pandora.Pattern.Object.Ringoid (Ringoid ((*)))
+import Pandora.Pattern.Object.Monoid (Monoid (zero))
+import Pandora.Pattern.Object.Quasiring (Quasiring (one))
 import Pandora.Paradigm.Structure.Ability.Monotonic (Monotonic (reduce))
 
 newtype Vector r a = Vector r
@@ -11,3 +14,12 @@ instance Monotonic a r => Monotonic a (Vector r a) where
 
 instance Semigroup r => Semigroup (Vector r a) where
 	Vector x + Vector y = Vector $ x + y
+
+instance Ringoid r => Ringoid (Vector r a) where
+	Vector x * Vector y = Vector $ x * y
+
+instance Monoid r => Monoid (Vector r a) where
+	zero = Vector zero
+
+instance Quasiring r => Quasiring (Vector r a) where
+	one = Vector one
