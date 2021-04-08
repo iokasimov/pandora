97000d0043bdbac00947adcad99af7ddf358b004
Murat Kasimov <iokasimov.m@gmail.com>
2021-04-08 19:49:03 +0300

    Define Semigroup instance for redefined Vector datatype

--- Pandora/Paradigm/Structure/Some/Vector.hs
+++ Pandora/Paradigm/Structure/Some/Vector.hs
@@ -1,7 +1,15 @@
 module Pandora.Paradigm.Structure.Some.Vector where
 
+import Pandora.Pattern.Category (($), (/))
+import Pandora.Pattern.Object.Semigroup (Semigroup ((+)))
 import Pandora.Paradigm.Primary.Functor.Product (type (:*:))
 
 data Vector r a where
 	Scalar :: a -> Vector a a
 	Vector :: a -> Vector r a -> Vector (a :*: r) a
+
+instance Semigroup a => Semigroup (Vector a a) where
+	Scalar x + Scalar y = Scalar $ x + y
+
+instance (Semigroup a, Semigroup r, Semigroup (a :*: r), Semigroup (Vector r a)) => Semigroup (Vector (a :*: r) a) where
+	Vector x xs + Vector y ys = Vector / x + y / xs + ys
