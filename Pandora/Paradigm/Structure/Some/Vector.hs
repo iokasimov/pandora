26e94b554fc1b148a3da5be65157aa7daa9387e7
Murat Kasimov <iokasimov.m@gmail.com>
2021-04-08 18:41:46 +0300

    Add actual value parameter to Vector to implement Semigroup instance

--- Pandora/Paradigm/Structure/Some/Vector.hs
+++ Pandora/Paradigm/Structure/Some/Vector.hs
@@ -1,8 +1,13 @@
 module Pandora.Paradigm.Structure.Some.Vector where
 
+import Pandora.Pattern.Category (($))
+import Pandora.Pattern.Object.Semigroup (Semigroup ((+)))
 import Pandora.Paradigm.Structure.Ability.Monotonic (Monotonic (reduce))
 
-data Vector a = forall r . Monotonic a r => Vector r
+newtype Vector r a = Vector r
 
-instance Monotonic a (Vector a) where
+instance Monotonic a r => Monotonic a (Vector r a) where
 	reduce f r (Vector x) = reduce f r x
+
+instance Semigroup r => Semigroup (Vector r a) where
+	Vector x + Vector y = Vector $ x + y
