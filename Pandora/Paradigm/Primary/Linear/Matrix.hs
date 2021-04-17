5a4ffeaaff56a4ac7753e8f5177855566965cc5e
Murat Kasimov <iokasimov.m@gmail.com>
2021-04-17 14:12:53 +0300

    Define Monoid instance for Matrix datatype

--- Pandora/Paradigm/Primary/Linear/Matrix.hs
+++ Pandora/Paradigm/Primary/Linear/Matrix.hs
@@ -4,9 +4,13 @@ module Pandora.Paradigm.Primary.Linear.Matrix where
 
 import Pandora.Pattern.Category (($))
 import Pandora.Pattern.Object.Semigroup (Semigroup ((+)))
+import Pandora.Pattern.Object.Monoid (Monoid (zero))
 import Pandora.Paradigm.Primary.Linear.Vector (Vector)
 
 newtype Matrix i j a = Matrix (Vector i (Vector j a))
 
-instance (Semigroup a, Semigroup (Vector i a), Semigroup (Vector i (Vector i a))) => Semigroup (Matrix i i a) where
+instance (Semigroup a, Semigroup (Vector i a), Semigroup (Vector i (Vector j a))) => Semigroup (Matrix i j a) where
 	~(Matrix x) + ~(Matrix y) = Matrix $ x + y
+
+instance (Monoid a, Monoid (Vector i a), Monoid (Vector i (Vector j a))) => Monoid (Matrix i j a) where
+	zero = Matrix zero
