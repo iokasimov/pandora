bee3b664c5c90e2e3db72b8344065cf524e17d7f
Murat Kasimov <iokasimov.m@gmail.com>
2021-04-17 14:10:21 +0300

    Define Semigroup instance for Matrix datatype

--- Pandora/Paradigm/Primary/Linear/Matrix.hs
+++ Pandora/Paradigm/Primary/Linear/Matrix.hs
@@ -1,5 +1,12 @@
+{-# LANGUAGE UndecidableInstances #-}
+
 module Pandora.Paradigm.Primary.Linear.Matrix where
 
+import Pandora.Pattern.Category (($))
+import Pandora.Pattern.Object.Semigroup (Semigroup ((+)))
 import Pandora.Paradigm.Primary.Linear.Vector (Vector)
 
 newtype Matrix i j a = Matrix (Vector i (Vector j a))
+
+instance (Semigroup a, Semigroup (Vector i a), Semigroup (Vector i (Vector i a))) => Semigroup (Matrix i i a) where
+	~(Matrix x) + ~(Matrix y) = Matrix $ x + y
