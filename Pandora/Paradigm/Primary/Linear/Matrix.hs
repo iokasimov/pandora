70c203c0aae4dfff50252eaa4d21d0971be64a7d
Murat Kasimov <iokasimov.m@gmail.com>
2021-04-18 15:02:18 +0300

    Define Setoid instance for Matrix datatype

--- Pandora/Paradigm/Primary/Linear/Matrix.hs
+++ Pandora/Paradigm/Primary/Linear/Matrix.hs
@@ -5,6 +5,7 @@ module Pandora.Paradigm.Primary.Linear.Matrix where
 import Pandora.Pattern.Category (($))
 import Pandora.Pattern.Object.Semigroup (Semigroup ((+)))
 import Pandora.Pattern.Object.Monoid (Monoid (zero))
+import Pandora.Pattern.Object.Setoid (Setoid ((==)))
 import Pandora.Paradigm.Primary.Linear.Vector (Vector)
 
 newtype Matrix i j a = Matrix (Vector i (Vector j a))
@@ -14,3 +15,6 @@ instance (Semigroup a, Semigroup (Vector i a), Semigroup (Vector i (Vector j a))
 
 instance (Monoid a, Monoid (Vector i a), Monoid (Vector i (Vector j a))) => Monoid (Matrix i j a) where
 	zero = Matrix zero
+
+instance (Setoid a, Setoid (Vector i a), Setoid (Vector i (Vector j a))) => Setoid (Matrix i j a) where
+	Matrix x == Matrix y = x == y
