708e77b094d971c1a1f39f88c1812fe8fbaff175
Murat Kasimov <iokasimov.m@gmail.com>
2021-04-21 17:32:49 +0300

    Define special instance for Substucture for combining lenses

--- Pandora/Paradigm/Structure/Ability/Substructure.hs
+++ Pandora/Paradigm/Structure/Ability/Substructure.hs
@@ -1,13 +1,16 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Pandora.Paradigm.Structure.Ability.Substructure where
 
-import Pandora.Pattern.Category ((.))
+import Pandora.Pattern.Category ((.), ($))
 import Pandora.Pattern.Functor.Covariant (Covariant ((<$>)))
 import Pandora.Pattern.Functor.Extractable (extract)
+import Pandora.Pattern.Functor.Pointable (point)
+import Pandora.Pattern.Functor.Divariant ((>->))
 import Pandora.Pattern.Transformer.Liftable (lift)
-import Pandora.Paradigm.Controlflow.Effect.Interpreted (run)
-import Pandora.Paradigm.Inventory.Optics (type (:~.))
+import Pandora.Paradigm.Controlflow.Effect.Interpreted (run, unite)
+import Pandora.Paradigm.Inventory.Optics (type (:~.), (|>))
 import Pandora.Paradigm.Primary.Functor.Tagged (Tagged)
 import Pandora.Paradigm.Schemes.TU (type (<:.>))
 
@@ -19,3 +22,9 @@ class Substructure f t where
 	sub x = extract . run <$> substructure @f (lift x)
 
 data Segment a = Tail a
+
+data (|>) (i :: * -> k) (j :: * -> k') a
+
+instance (Covariant t, Covariant (Substructural i t), Substructure i t, Substructure j (Substructural i t)) => Substructure (i |> j) t where
+	type Substructural (i |> j) t = Substructural j (Substructural i t)
+	substructure = extract . run >-> (unite . point <$>) $ sub @i |> sub @j
