0696f7889bfda70dad34c5a6576861ef45fd7aef
Murat Kasimov <iokasimov.m@gmail.com>
2021-04-28 18:45:59 +0300

    Define Interpreted instance for PQ_ joint schema

--- Pandora/Paradigm/Schemes/PQ_.hs
+++ Pandora/Paradigm/Schemes/PQ_.hs
@@ -1,3 +1,10 @@
 module Pandora.Paradigm.Schemes.PQ_ where
 
+import Pandora.Paradigm.Controlflow.Effect.Interpreted (Interpreted (Primary, run, unite))
+
 newtype PQ_ p q a b = PQ_ (p a (q b a))
+
+instance Interpreted (PQ_ p q a) where
+	type Primary (PQ_ p q a) b = p a (q b a)
+	run ~(PQ_ x) = x
+	unite = PQ_
