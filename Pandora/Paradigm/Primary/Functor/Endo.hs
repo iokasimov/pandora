18b9c3fc5b8f00727dddbc6f950fc88c6f1cef00
Murat Kasimov <iokasimov.m@gmail.com>
2021-04-30 18:24:56 +0300

    Define Interpreted instance for Endo

--- Pandora/Paradigm/Primary/Functor/Endo.hs
+++ Pandora/Paradigm/Primary/Functor/Endo.hs
@@ -2,14 +2,21 @@ module Pandora.Paradigm.Primary.Functor.Endo where
 
 import Pandora.Pattern.Category (identity, (.), (#))
 import Pandora.Pattern.Functor.Invariant (Invariant ((>-<)))
+import Pandora.Pattern.Functor.Divariant ((>->))
 import Pandora.Pattern.Object.Semigroup (Semigroup ((+)))
 import Pandora.Pattern.Object.Monoid (Monoid (zero))
+import Pandora.Paradigm.Controlflow.Effect.Interpreted (Interpreted (Primary, run, unite, (||=)))
 import Pandora.Paradigm.Primary.Functor.Function ()
 
 newtype Endo a = Endo { endo :: a -> a }
 
+instance Interpreted Endo where
+	type Primary Endo a = a -> a
+	run ~(Endo x) = x
+	unite = Endo
+
 instance Invariant Endo where
-	f >-< g = \(Endo x) -> Endo (f . x . g)
+	f >-< g = (g >-> f ||=)
 
 instance Semigroup (Endo a) where
 	Endo f + Endo g = Endo # g . f
