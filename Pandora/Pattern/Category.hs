231453432aa359ebf6cbed7b24d7a9088480dd47
Murat Kasimov <iokasimov.m@gmail.com>
2021-05-16 07:38:25 +0300

    Add laws to Catagory typeclass

--- Pandora/Pattern/Category.hs
+++ Pandora/Pattern/Category.hs
@@ -4,6 +4,13 @@ infixl 2 #
 infixr 0 $
 infixr 9 .
 
+{- |
+> When providing a new instance, you should ensure it satisfies:
+> * Left identity: identity . f ≡ f
+> * Right identity: f . identity ≡ f
+> * Associativity: f . (g . h) ≡ (f . g) . h
+-}
+
 class Category (m :: * -> * -> *) where
 	identity :: m a a
 	(.) :: m b c -> m a b -> m a c
