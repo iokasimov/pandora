0db356707b3d131b8267ebd39f29ba7d8abbdd12
Murat Kasimov <iokasimov.m@gmail.com>
2021-05-19 09:13:48 +0300

    Define Category instance for Prism

--- Pandora/Paradigm/Inventory/Optics.hs
+++ Pandora/Paradigm/Inventory/Optics.hs
@@ -5,18 +5,20 @@ module Pandora.Paradigm.Inventory.Optics where
 import Pandora.Pattern.Category (Category (identity, (.), ($)))
 import Pandora.Pattern.Functor.Covariant ((<$>), (<$))
 import Pandora.Pattern.Functor.Extractable (Extractable (extract))
+import Pandora.Pattern.Functor.Bindable ((>>=))
 import Pandora.Pattern.Functor.Representable (Representable (Representation, (<#>), tabulate))
 import Pandora.Pattern.Functor.Divariant ((>->))
 import Pandora.Pattern.Functor.Invariant (Invariant ((<$<)))
 import Pandora.Pattern.Object.Setoid (Setoid ((==)))
 import Pandora.Paradigm.Controlflow.Effect.Interpreted (run, (||=))
-import Pandora.Paradigm.Primary.Functor.Maybe (Maybe)
-import Pandora.Paradigm.Primary.Functor.Product (Product ((:*:)))
+import Pandora.Paradigm.Primary.Functor.Function ((!))
+import Pandora.Paradigm.Primary.Functor.Maybe (Maybe (Just, Nothing))
+import Pandora.Paradigm.Primary.Functor.Product (Product ((:*:)), attached)
 import Pandora.Paradigm.Primary.Transformer.Flip (Flip)
 import Pandora.Paradigm.Primary.Object.Boolean ((?))
 import Pandora.Paradigm.Inventory.Store (Store (Store), position, look, retrofit)
 import Pandora.Paradigm.Schemes.PQ_ (PQ_ (PQ_))
-import Pandora.Paradigm.Schemes.P_T (P_T)
+import Pandora.Paradigm.Schemes.P_T (P_T (P_T))
 
 infixr 0 :-.
 infixr 0 :~.
@@ -53,6 +55,13 @@ represent :: (Representable t, Setoid (Representation t)) => Representation t ->
 represent r = PQ_ $ \x -> Store $ r <#> x :*: \new -> tabulate (\r' -> r' == r ? new $ r' <#> x)
 
 type Prism = PQ_ (->) (P_T Store Maybe)
+
+instance Category Prism where
+	identity = PQ_ $ \src -> P_T . Store $ Just src :*: (src !)
+	PQ_ to . PQ_ from = PQ_ $ \src -> P_T $
+		case attached (run (run (from src))) of
+			Just focused -> src <$ run (to focused)
+			Nothing -> Store $ Nothing :*: (src !)
 
 preview :: Prism src tgt -> src -> Maybe tgt
 preview prism = position . run . run prism
