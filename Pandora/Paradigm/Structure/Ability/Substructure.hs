4880322957e5b6eaa069c52d5707719c296b6b8a
Murat Kasimov <iokasimov.m@gmail.com>
2021-05-21 16:13:26 +0300

    Rename type parameters in Substructure typeclass

--- Pandora/Paradigm/Structure/Ability/Substructure.hs
+++ Pandora/Paradigm/Structure/Ability/Substructure.hs
@@ -9,26 +9,17 @@ import Pandora.Pattern.Functor.Divariant ((>->))
 import Pandora.Pattern.Transformer.Liftable (lift)
 import Pandora.Pattern.Transformer.Lowerable (lower)
 import Pandora.Paradigm.Controlflow.Effect.Interpreted ((||=))
-import Pandora.Paradigm.Inventory.Optics (type (:~.), type (#=@))
+import Pandora.Paradigm.Inventory.Optics (type (#=@))
+import Pandora.Paradigm.Primary.Functor.Identity (Identity)
 import Pandora.Paradigm.Primary.Functor.Tagged (Tagged)
 import Pandora.Paradigm.Schemes.TU (type (<:.>))
-
--- class Substructure f t where
--- 	type Substructural (f :: k) (t :: * -> *) :: * -> *
--- 	substructure :: Tagged f <:.> t :~. Substructural f t
---
--- 	sub :: Covariant t => t :~. Substructural f t
--- 	sub = lift >-> ((lower <$>) ||=) ||= substructure @f @t
 
 data Segment a = Root a | Tail a
 
 -- FIXME: uncomment this type synonymous as soon as I realize what to do with Available
 -- type Substructured i source target = (Substructure i source, Substructural i source ~ target)
 
-class Substructure f t where
-	type Available (f :: k) (t :: * -> *) :: * -> *
-	type Substance (f :: k) (t :: * -> *) :: * -> *
-	substructure :: (Tagged f <:.> t) #=@ Substance f t := Available f t
-
-	sub :: Covariant t => t #=@ Substance f t := Available f t
-	sub = lift >-> ((lower <$>) ||=) ||= substructure @f @t
+class Substructure (segment :: k) (structure :: * -> *) where
+	type Available segment structure :: * -> *
+	type Substance segment structure :: * -> *
+	substructure :: (Tagged segment <:.> structure) #=@ Substance segment structure := Available segment structure
