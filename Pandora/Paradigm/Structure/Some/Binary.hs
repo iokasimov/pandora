e35094f70f29fd9fab2eb6603fc5b73166356aae
Murat Kasimov <iokasimov.m@gmail.com>
2021-05-23 08:17:32 +0300

    [WIP] Redefine Substructure with Optics: fix instances in Binary module

--- Pandora/Paradigm/Structure/Some/Binary.hs
+++ Pandora/Paradigm/Structure/Some/Binary.hs
@@ -4,7 +4,7 @@ module Pandora.Paradigm.Structure.Some.Binary where
 
 import Pandora.Core.Functor (type (:.), type (:=), type (:=>))
 import Pandora.Pattern.Category (identity, (.), ($), (#))
-import Pandora.Pattern.Functor.Covariant (Covariant ((<$>), ($$>)))
+import Pandora.Pattern.Functor.Covariant (Covariant ((<$>), ($$>), ($$$>)))
 import Pandora.Pattern.Functor.Extractable (extract)
 import Pandora.Pattern.Functor.Avoidable (empty)
 import Pandora.Pattern.Functor.Bindable ((>>=))
@@ -27,16 +27,16 @@ import Pandora.Paradigm.Primary.Functor.Product (Product ((:*:)), type (:*:), at
 import Pandora.Paradigm.Primary.Functor.Wye (Wye (End, Left, Right, Both))
 import Pandora.Paradigm.Primary.Transformer.Construction (Construction (Construct), deconstruct)
 import Pandora.Paradigm.Schemes (TU (TU), T_U (T_U), PQ_ (PQ_), P_T (P_T), type (<:.>), type (<:.:>))
-import Pandora.Paradigm.Controlflow.Effect.Interpreted (run)
+import Pandora.Paradigm.Controlflow.Effect.Interpreted (run, (=||))
 import Pandora.Paradigm.Inventory.Store (Store (Store))
-import Pandora.Paradigm.Inventory.Optics (over, view)
+import Pandora.Paradigm.Inventory.Optics (over, over', view, view')
 import Pandora.Paradigm.Structure.Ability.Nonempty (Nonempty)
 import Pandora.Paradigm.Structure.Ability.Nullable (Nullable (null))
 import Pandora.Paradigm.Structure.Ability.Measurable (Measurable (Measural, measurement), Scale (Heighth), measure)
 import Pandora.Paradigm.Structure.Ability.Monotonic (Monotonic (resolve))
-import Pandora.Paradigm.Structure.Ability.Morphable (Morphable (Morphing, morphing)
-	, Morph (Rotate, Into, Insert, Lookup, Vary, Key, Element), Vertical (Up, Down), morph, premorph, lookup)
-import Pandora.Paradigm.Structure.Ability.Substructure (Substructure (Substructural, substructure), Segment (Root), sub)
+import Pandora.Paradigm.Structure.Ability.Morphable (Morphable (Morphing, morphing), morph, premorph
+	, Morph (Rotate, Into, Insert, Lookup, Vary, Key, Element), Vertical (Up, Down), collate, lookup, vary)
+import Pandora.Paradigm.Structure.Ability.Substructure (Substructure (Available, Substance, substructure), Segment (Root), sub)
 import Pandora.Paradigm.Structure.Ability.Zipper (Zipper)
 import Pandora.Paradigm.Structure.Modification.Prefixed (Prefixed (Prefixed))
 
@@ -52,8 +52,11 @@ instance Morphable Insert Binary where
 	type Morphing Insert Binary = (Identity <:.:> Comparison := (:*:)) <:.:> Binary := (->)
 	morphing (run . premorph -> Nothing) = T_U $ \(T_U (Identity x :*: _)) -> lift $ leaf x
 	morphing (run . premorph -> Just ne) = T_U $ \(T_U (Identity x :*: Convergence f)) ->
-		let continue xs = run # morph @Insert xs $ twosome # Identity x # Convergence f
-		in lift $ f x # extract ne & order # ne # over (sub @Left) continue ne # over (sub @Right) continue ne
+		let continue xs = run # morph @Insert @(Nonempty Binary) xs $ twosome # Identity x # Convergence f in
+		let change = Just . resolve continue (leaf x) in
+		lift $ f x # extract ne & order # ne
+			# over' (sub @Left) change ne
+			# over' (sub @Right) change ne
 
 instance Measurable Heighth Binary where
 	type Measural Heighth Binary a = Numerator
@@ -64,15 +67,17 @@ instance Nullable Binary where
 	null = Predicate $ \case { TU Nothing -> True ; _ -> False }
 
 instance Substructure Left Binary where
-	type Substructural Left Binary = Binary
-	substructure = PQ_ $ \bintree -> P_T $ case run . extract . run # bintree of
-		Nothing -> Store $ Identity empty :*: lift . identity . extract
+	type Available Left Binary = Maybe
+	type Substance Left Binary = Construction Wye
+	substructure = PQ_ $ \bintree -> P_T $ case run . lower # bintree of
+		Nothing -> Store $ Nothing :*: lift . TU
 		Just tree -> lift . lift <$> run (run (sub @Left) tree)
 
 instance Substructure Right Binary where
-	type Substructural Right Binary = Binary
+	type Available Right Binary = Maybe
+	type Substance Right Binary = Construction Wye
 	substructure = PQ_ $ \bintree -> P_T $ case run . extract . run # bintree of
-		Nothing -> Store $ Identity empty :*: lift . identity . extract
+		Nothing -> Store $ Nothing :*: lift . TU
 		Just tree -> lift . lift <$> run (run (sub @Right) tree)
 
 -------------------------------------- Non-empty binary tree ---------------------------------------
@@ -87,10 +92,10 @@ instance Morphable Insert (Construction Wye) where
 	type Morphing Insert (Construction Wye) = (Identity <:.:> Comparison := (:*:)) <:.:> Construction Wye := (->)
 	morphing (premorph -> nonempty_list) = T_U $ \(T_U (Identity x :*: Convergence f)) ->
 		let continue xs = run # morph @Insert @(Nonempty Binary) xs $ twosome # Identity x # Convergence f in
-		let change = lift . resolve continue (leaf x) . run in
+		let change = Just . resolve continue (leaf x) in
 		order # nonempty_list
-			# over (sub @Left) change nonempty_list
-			# over (sub @Right) change nonempty_list
+			# over' (sub @Left) change nonempty_list
+			# over' (sub @Right) change nonempty_list
 			# f x (extract nonempty_list)
 
 instance Measurable Heighth (Construction Wye) where
@@ -103,25 +108,28 @@ instance Measurable Heighth (Construction Wye) where
 		in lm <=> rm & order lm rm lm
 
 instance Substructure Root (Construction Wye) where
-	type Substructural Root (Construction Wye) = Identity
+	type Available Root (Construction Wye) = Identity
+	type Substance Root (Construction Wye) = Identity
 	substructure = PQ_ $ \bintree -> P_T $ case lower bintree of
 		Construct x xs -> Store $ Identity (Identity x) :*: lift . (Construct % xs) . extract . extract
 
 instance Substructure Left (Construction Wye) where
-	type Substructural Left (Construction Wye) = Binary
+	type Available Left (Construction Wye) = Maybe
+	type Substance Left (Construction Wye) = Construction Wye
 	substructure = PQ_ $ \bintree -> P_T $ case extract # run bintree of
-		Construct x End -> Store $ Identity empty :*: lift . resolve (Construct x . Left) (leaf x) . run . extract
-		Construct x (Left lst) -> Store $ Identity (lift lst) :*: lift . Construct x . resolve Left End . run . extract
-		Construct x (Right rst) -> Store $ Identity empty :*: lift . Construct x . resolve (Both % rst) (Right rst) . run . extract
-		Construct x (Both lst rst) -> Store $ Identity (lift lst) :*: lift . Construct x . resolve (Both % rst) (Right rst) . run . extract
+		Construct x End -> Store $ Nothing :*: lift . resolve (Construct x . Left) (leaf x)
+		Construct x (Left lst) -> Store $ Just lst :*: lift . Construct x . resolve Left End
+		Construct x (Right rst) -> Store $ Nothing :*: lift . Construct x . resolve (Both % rst) (Right rst)
+		Construct x (Both lst rst) -> Store $ Just lst :*: lift . Construct x . resolve (Both % rst) (Right rst)
 
 instance Substructure Right (Construction Wye) where
-	type Substructural Right (Construction Wye) = Binary
+	type Available Right (Construction Wye) = Maybe
+	type Substance Right (Construction Wye) = Construction Wye
 	substructure = PQ_ $ \bintree -> P_T $ case extract # run bintree of
-		Construct x End -> Store $ Identity empty :*: lift . resolve (Construct x . Right) (leaf x) . run . extract
-		Construct x (Left lst) -> Store $ Identity empty :*: lift . Construct x . resolve (Both lst) (Left lst) . run . extract
-		Construct x (Right rst) -> Store $ Identity (lift rst) :*: lift . Construct x . resolve Right End . run . extract
-		Construct x (Both lst rst) -> Store $ Identity (lift rst) :*: lift . Construct x . resolve (Both lst) (Left lst) . run . extract
+		Construct x End -> Store $ Nothing :*: lift . resolve (Construct x . Right) (leaf x)
+		Construct x (Left lst) -> Store $ Nothing :*: lift . Construct x . resolve (Both lst) (Left lst)
+		Construct x (Right rst) -> Store $ Just rst :*: lift . Construct x . resolve Right End
+		Construct x (Both lst rst) -> Store $ Just rst :*: lift . Construct x . resolve (Both lst) (Left lst)
 
 -------------------------------------- Prefixed binary tree ----------------------------------------
 
@@ -129,17 +137,19 @@ instance Chain k => Morphable (Lookup Key) (Prefixed Binary k) where
 	type Morphing (Lookup Key) (Prefixed Binary k) = (->) k <:.> Maybe
 	morphing (run . run . premorph -> Nothing) = lift Nothing
 	morphing (run . run . premorph -> Just tree) = TU $ \key ->
-		let root = extract tree in key <=> attached root & order # Just (extract root)
-			# lookup @Key key (Prefixed $ view # sub @Left # tree)
-			# lookup @Key key (Prefixed $ view # sub @Right # tree)
+		let root = extract tree in key <=> attached root & order (Just # extract root)
+			(view' # sub @Left # tree >>= lookup @Key key . Prefixed)
+			(view' # sub @Right # tree >>= lookup @Key key . Prefixed)
 
 instance Chain k => Morphable (Vary Element) (Prefixed Binary k) where
 	type Morphing (Vary Element) (Prefixed Binary k) = (Product k <:.> Identity) <:.:> Prefixed Binary k := (->)
 	morphing (run . run . premorph -> Nothing) = T_U $ \(TU (key :*: Identity value)) -> Prefixed . lift . leaf $ key :*: value
 	morphing (run . run . premorph -> Just tree) = T_U $ \(TU (key :*: Identity value)) ->
-		let continue xs = run $ run # morph @(Vary Element) (Prefixed xs) # TU (key :*: Identity value)
+		let continue = ((vary @Element @k @_ @(Prefixed Binary _) key value =||) =||)
 		in let root = extract tree in Prefixed . lift $ key <=> attached root & order
-			# over (sub @Root) ($$> value) tree # over (sub @Left) continue tree # over (sub @Right) continue tree
+			# over' (sub @Root) ($$$> value) tree
+			# over' (sub @Left) continue tree
+			# over' (sub @Right) continue tree
 
 ---------------------------------- Prefixed non-empty binary tree ----------------------------------
 
@@ -147,8 +157,8 @@ instance Chain key => Morphable (Lookup Key) (Prefixed (Construction Wye) key) w
 	type Morphing (Lookup Key) (Prefixed (Construction Wye) key) = (->) key <:.> Maybe
 	morphing (run . premorph -> Construct x xs) = TU $ \key ->
 		key <=> attached x & order (Just # extract x)
-			(view # sub @Left # xs >>= lookup @Key key . Prefixed)
-			(view # sub @Right # xs >>= lookup @Key key . Prefixed)
+			(view' # sub @Left # xs >>= lookup @Key key . Prefixed . extract)
+			(view' # sub @Right # xs >>= lookup @Key key . Prefixed . extract)
 
 -------------------------------------- Zipper of binary tree ---------------------------------------
 
