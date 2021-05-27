69960f8bf36906635c43ac74153e26ce543252ef
Murat Kasimov <iokasimov.m@gmail.com>
2021-05-27 07:23:16 +0300

    Define Interpreted instance for PTU joint schema

--- Pandora/Paradigm/Schemes/PTU.hs
+++ Pandora/Paradigm/Schemes/PTU.hs
@@ -1,3 +1,10 @@
 module Pandora.Paradigm.Schemes.PTU where
 
+import Pandora.Paradigm.Controlflow.Effect.Interpreted (Interpreted (Primary, run, unite))
+
 newtype PTU p t u a b = PTU (p (t a) (u b))
+
+instance Interpreted (PTU p t u a) where
+	type Primary (PTU p t u a) b = p (t a) (u b)
+	run ~(PTU x) = x
+	unite = PTU
