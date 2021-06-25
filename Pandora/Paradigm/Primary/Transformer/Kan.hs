78452d0945f625e99c5a37f93c409b61e114306d
Murat Kasimov <iokasimov.m@gmail.com>
2021-06-25 17:02:09 +0300

    Define Covariant_ and Contravariant_ instances for Kan extensions

--- Pandora/Paradigm/Primary/Transformer/Kan.hs
+++ Pandora/Paradigm/Primary/Transformer/Kan.hs
@@ -1,8 +1,8 @@
 module Pandora.Paradigm.Primary.Transformer.Kan where
 
 import Pandora.Pattern.Category ((.), ($))
-import Pandora.Pattern.Functor.Contravariant (Contravariant ((>$<)))
-import Pandora.Pattern.Functor.Covariant (Covariant ((<$>)))
+import Pandora.Pattern.Functor.Contravariant (Contravariant_ ((->$<-)))
+import Pandora.Pattern.Functor.Covariant (Covariant_ ((-<$>-)))
 import Pandora.Paradigm.Controlflow.Effect.Interpreted (Interpreted (Primary, run, unite))
 import Pandora.Paradigm.Primary.Functor.Wye (Wye (Left, Right))
 
@@ -10,8 +10,8 @@ data family Kan (v :: * -> k) (t :: * -> *) (u :: * -> *) b a
 
 data instance Kan Left t u b a = Lan ((t b -> a) -> u b)
 
-instance Contravariant (Kan Left t u b) where
-	f >$< Lan x = Lan $ x . (f .)
+instance Contravariant_ (Kan Left t u b) (->) (->) where
+	f ->$<- Lan x = Lan $ x . (f .)
 
 instance Interpreted (Kan Left t u b) where
 	type Primary (Kan Left t u b) a = (t b -> a) -> u b
@@ -20,8 +20,8 @@ instance Interpreted (Kan Left t u b) where
 
 data instance Kan Right t u b a = Ran ((a -> t b) -> u b)
 
-instance Covariant (Kan Right t u b) where
-	f <$> Ran x = Ran $ x . (. f)
+instance Covariant_ (Kan Right t u b) (->) (->) where
+	f -<$>- Ran x = Ran $ x . (. f)
 
 instance Interpreted (Kan Right t u b) where
 	type Primary (Kan Right t u b) a = (a -> t b) -> u b
