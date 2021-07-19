b6857cf347e76c5204acd4d2ae6da4b6a9160d3e
Murat Kasimov <iokasimov.m@gmail.com>
2021-07-19 17:27:36 +0300

    Define Traversable_ instance for Product

--- Pandora/Paradigm/Primary/Algebraic.hs
+++ Pandora/Paradigm/Primary/Algebraic.hs
@@ -1,13 +1,15 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
 module Pandora.Paradigm.Primary.Algebraic (module Exports, (-<*>-)) where
 
 import Pandora.Paradigm.Primary.Algebraic.Exponential as Exports
 import Pandora.Paradigm.Primary.Algebraic.Product as Exports
 
 import Pandora.Pattern.Category (($))
-import Pandora.Pattern.Functor.Covariant (Covariant ((<$>)))
+import Pandora.Pattern.Functor.Covariant (Covariant ((<$>)), Covariant_ ((-<$>-)))
 import Pandora.Pattern.Functor.Extractable (Extractable (extract))
 import Pandora.Pattern.Functor.Applicative (Applicative_ (multiply))
-import Pandora.Pattern.Functor.Traversable (Traversable ((->>)))
+import Pandora.Pattern.Functor.Traversable (Traversable ((->>)), Traversable_ ((-<<--)))
 import Pandora.Pattern.Functor.Adjoint (Adjoint ((-|), (|-)))
 
 infixl 4 -<*>-
@@ -17,6 +19,9 @@ instance Applicative_ ((->) e) (:*:) (->) (->) where
 
 instance Traversable ((:*:) s) where
 	x ->> f = (attached x :*:) <$> f (extract x)
+
+instance Traversable_ ((:*:) s) (->) (->) where
+	f -<<-- x = (attached x :*:) -<$>- f (extract x)
 
 instance Adjoint ((:*:) s) ((->) s) (->) (->) where
 	(-|) :: ((s :*: a) -> b) -> a -> (s -> b)
