0085797014c93120584312c946c3425f176614f0
Murat Kasimov <iokasimov.m@gmail.com>
2021-09-30 11:13:08 +0300

    Uncomment Transformer type family

--- Pandora/Paradigm/Controlflow/Effect/Transformer.hs
+++ Pandora/Paradigm/Controlflow/Effect/Transformer.hs
@@ -1,10 +1,10 @@
-module Pandora.Paradigm.Controlflow.Effect.Transformer (module Exports) where
+module Pandora.Paradigm.Controlflow.Effect.Transformer (module Exports, Transformer) where
 
 import Pandora.Paradigm.Controlflow.Effect.Transformer.Comonadic as Exports
 import Pandora.Paradigm.Controlflow.Effect.Transformer.Monadic as Exports
 
---import Pandora.Pattern.Functor (Monad, Comonad)
+import Pandora.Pattern.Functor (Monad, Comonad)
 
---type family Transformer c t where
---	Transformer Monad t = Monadic t
---	Transformer Comonad t = Comonadic t
+type family Transformer c t where
+	Transformer Monad t = Monadic t
+	Transformer Comonad t = Comonadic t
