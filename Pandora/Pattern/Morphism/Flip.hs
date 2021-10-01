daac973521c6243f172c321e9e3c0cd606ae7451
Murat Kasimov <iokasimov.m@gmail.com>
2021-10-01 13:13:38 +0300

    Add Covariant and Contravariant instances with Straight and Flip

--- Pandora/Pattern/Morphism/Flip.hs
+++ Pandora/Pattern/Morphism/Flip.hs
@@ -2,6 +2,8 @@ module Pandora.Pattern.Morphism.Flip where
 
 import Pandora.Pattern.Semigroupoid (Semigroupoid ((.)))
 import Pandora.Pattern.Category (Category (identity))
+import Pandora.Pattern.Functor.Covariant (Covariant ((<$>)))
+import Pandora.Pattern.Functor.Contravariant (Contravariant ((>$<)))
 import Pandora.Core.Appliable (Appliable ((!)))
 
 newtype Flip (v :: * -> * -> *) a e = Flip (v e a)
@@ -11,6 +13,9 @@ instance Semigroupoid m => Semigroupoid (Flip m) where
 
 instance Category m => Category (Flip m) where
 	identity = Flip identity
+
+instance (Category m, Covariant m m t) => Contravariant (Flip m) m t where
+	(>$<) (Flip f) = (<$>) f
 
 instance Appliable (Flip m) b c m c b where
 	(!) (Flip m) = m
--- Pandora/Pattern/Morphism/Straight.hs
+++ Pandora/Pattern/Morphism/Straight.hs
@@ -2,6 +2,7 @@ module Pandora.Pattern.Morphism.Straight where
 
 import Pandora.Pattern.Semigroupoid (Semigroupoid ((.)))
 import Pandora.Pattern.Category (Category (identity))
+import Pandora.Pattern.Functor.Covariant (Covariant ((<$>)))
 import Pandora.Core.Appliable (Appliable ((!)))
 
 newtype Straight (v :: * -> * -> *) a e = Straight (v a e)
@@ -11,6 +12,9 @@ instance Semigroupoid m => Semigroupoid (Straight m) where
 
 instance Category m => Category (Straight m) where
 	identity = Straight identity
+
+instance Covariant m m t => Covariant (Straight m) m t where
+	(<$>) (Straight f) = (<$>) f
 
 instance Appliable (Straight m) c b m c b where
 	(!) (Straight m) = m
