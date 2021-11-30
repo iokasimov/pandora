4a9d5b41d364b35d6bb143ded2ccdca022fe0afd
Murat Kasimov <iokasimov.m@gmail.com>
2021-11-30 10:50:48 +0300

    Refactor a bit

--- Pandora/Paradigm/Inventory.hs
+++ Pandora/Paradigm/Inventory.hs
@@ -11,7 +11,6 @@ import Pandora.Paradigm.Inventory.Equipment as Exports
 import Pandora.Paradigm.Inventory.Environment as Exports
 import Pandora.Paradigm.Inventory.Accumulator as Exports
 
-import Pandora.Core.Functor (type (~>))
 import Pandora.Pattern.Semigroupoid ((.))
 import Pandora.Pattern.Category (($), (#), identity)
 import Pandora.Pattern.Functor.Adjoint (Adjoint ((-|), (|-)))
@@ -38,18 +37,20 @@ instance Adjoint (->) (->) (Equipment e) (Environment e) where
 	f -| x = Environment $ f . Equipment -| x
 	g |- x = run . g |- run x
 
-zoom :: Stateful bg t => Lens Identity bg ls -> State ls ~> t
-zoom lens less = let restruct to = (to . Identity <-> identity @(->)) . run less . extract @Identity
-	in adapt . State $ (restruct |-) . run . run lens
+zoom :: forall bg ls t a . Stateful bg t => Convex Lens bg ls -> State ls a -> t a
+zoom lens less = adapt . State $ (restruct |-) . run . run lens where
 
-(=<>) :: Stateful src t => Lens mode src tgt -> mode tgt -> t src
+	restruct :: (Identity ls -> bg) -> Identity ls -> bg :*: a
+	restruct to = (to . Identity <-> identity @(->)) . run less . extract @Identity
+
+(=<>) :: Stateful src t => Lens available src tgt -> available tgt -> t src
 lens =<> new = modify $ set lens new
 
-(~<>) :: Stateful src t => Lens mode src tgt -> (mode tgt -> mode tgt) -> t src
+(~<>) :: Stateful src t => Lens available src tgt -> (available tgt -> available tgt) -> t src
 lens ~<> f = modify $ over lens f
 
 magnify :: forall bg ls t . (Accessible ls bg, Stateful bg t) => t ls
 magnify = zoom @bg # access @ls @bg # current
 
 adjust :: forall bg ls t . (Accessible ls bg, Stateful bg t) => (ls -> ls) -> t ls
-adjust = zoom @bg (access @ls @bg) . modify
+adjust = zoom @bg (access @ls) . modify
