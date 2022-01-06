85bb2a84b39dd28f605658be4dbe6718134dcba4
Murat Kasimov <iokasimov.m@gmail.com>
2022-01-06 11:52:22 +0300

    Fix module names in Inventory.Ability

--- Pandora/Paradigm/Inventory/Ability/Modifiable.hs
+++ Pandora/Paradigm/Inventory/Ability/Modifiable.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
-module Pandora.Paradigm.Inventory.Some.Ability.Modifiable where
+module Pandora.Paradigm.Inventory.Ability.Modifiable where
 
-import Pandora.Paradigm.Inventory.Some.Ability.Viewable (Viewable)
-import Pandora.Paradigm.Inventory.Some.Ability.Replaceable (Replaceable)
+import Pandora.Paradigm.Inventory.Ability.Viewable (Viewable)
+import Pandora.Paradigm.Inventory.Ability.Replaceable (Replaceable)
 
 class (Viewable i, Replaceable i) => Modifiable i where
-	type Modification i l r :: *
-	modify_ :: Modification i l r
+	type Modification i e r :: *
+	modify_ :: Modification i e r
--- Pandora/Paradigm/Inventory/Ability/Replaceable.hs
+++ Pandora/Paradigm/Inventory/Ability/Replaceable.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
-module Pandora.Paradigm.Inventory.Some.Ability.Replaceable where
+module Pandora.Paradigm.Inventory.Ability.Replaceable where
 
 class Replaceable i where
-	type family Replacement i l r :: *
-	replace_:: Replacement i l r
+	type family Replacement i e r :: *
+	replace_:: Replacement i e r
 
