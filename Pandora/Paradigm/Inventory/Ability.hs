c784ca887e1610e418af9cd3cdb75ab024808b6a
Murat Kasimov <iokasimov.m@gmail.com>
2022-01-09 11:29:16 +0300

    Fix Inventory.Ability module name

--- Pandora/Paradigm/Inventory/Ability.hs
+++ Pandora/Paradigm/Inventory/Ability.hs
@@ -1,4 +1,4 @@
-module Pandora.Paradigm.Inventory.Some.Ability (module Exports) where
+module Pandora.Paradigm.Inventory.Ability (module Exports) where
 
 import Pandora.Paradigm.Inventory.Ability.Modifiable as Exports
 import Pandora.Paradigm.Inventory.Ability.Settable as Exports
