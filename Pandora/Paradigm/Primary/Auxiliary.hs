6d06e0ec4cba84df348cabd49940e9dcd36fb43f
Murat Kasimov <iokasimov.m@gmail.com>
2022-04-19 08:41:59 +0700

    Define Covariant instances for Vertical and Horizontal

--- Pandora/Paradigm/Primary/Auxiliary.hs
+++ Pandora/Paradigm/Primary/Auxiliary.hs
@@ -1,5 +1,17 @@
 module Pandora.Paradigm.Primary.Auxiliary where
 
+import Pandora.Pattern.Category ((<--))
+import Pandora.Pattern.Functor.Covariant (Covariant ((<-|-)))
+import Pandora.Paradigm.Algebraic.Exponential ()
+
 data Vertical a = Up a | Down a
 
+instance Covariant (->) (->) Vertical where
+	f <-|- Up x = Up <-- f x
+	f <-|- Down x = Down <-- f x
+
 data Horizontal a = Left a | Right a
+
+instance Covariant (->) (->) Horizontal where
+	f <-|- Left x = Left <-- f x
+	f <-|- Right x = Right <-- f x
