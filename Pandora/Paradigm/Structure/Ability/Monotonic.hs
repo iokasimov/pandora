cc31e4daf8dd794257ee326fc78d5df1f206c832
Murat Kasimov <iokasimov.m@gmail.com>
2022-05-10 07:18:50 +0700

    Define Monotonic instances for :+:

--- Pandora/Paradigm/Structure/Ability/Monotonic.hs
+++ Pandora/Paradigm/Structure/Ability/Monotonic.hs
@@ -3,6 +3,7 @@ module Pandora.Paradigm.Structure.Ability.Monotonic where
 import Pandora.Pattern.Category ((<----))
 import Pandora.Pattern.Kernel (constant)
 import Pandora.Paradigm.Algebraic.Exponential ((.:..))
+import Pandora.Paradigm.Algebraic.Sum ((:+:) (Option, Adoption))
 
 class Monotonic a e where
 	{-# MINIMAL reduce #-}
@@ -14,3 +15,11 @@ class Monotonic a e where
 
 instance Monotonic a a where
 	reduce f r x = f x r
+
+instance Monotonic a (o :+: a) where
+	reduce fun def (Adoption x) = fun x def
+	reduce _ def (Option x) = def
+
+instance Monotonic o (o :+: a) where
+	reduce fun def (Option x) = fun x def
+	reduce _ def (Adoption x) = def
