bb10770023960da78c4d8c457c5939c88708267f
Murat Kasimov <iokasimov.m@gmail.com>
2022-05-15 17:22:04 +0700

    Define Slidable Left/Right instances for Turnover Tape

--- Pandora/Paradigm/Structure/Modification/Tape.hs
+++ Pandora/Paradigm/Structure/Modification/Tape.hs
@@ -2,9 +2,9 @@
 {-# LANGUAGE UndecidableInstances #-}
 module Pandora.Paradigm.Structure.Modification.Tape where
 
-import Pandora.Core.Functor (type (>), type (>>>))
+import Pandora.Core.Functor (type (>), type (<), type (>>>))
 import Pandora.Core.Impliable (Impliable (Arguments, imply))
-import Pandora.Core.Interpreted (run, (=#-))
+import Pandora.Core.Interpreted (run, (=#-), (<~~))
 import Pandora.Pattern.Semigroupoid ((.))
 import Pandora.Pattern.Category ((<--), (<---), (<----), (<------))
 import Pandora.Pattern.Kernel (constant)
@@ -16,23 +16,25 @@ import Pandora.Pattern.Functor.Monoidal (Monoidal)
 import Pandora.Pattern.Transformer.Liftable (lift)
 import Pandora.Pattern.Transformer.Lowerable (lower)
 import Pandora.Paradigm.Algebraic.Exponential (type (-->), (%))
-import Pandora.Paradigm.Algebraic.Product ((:*:) ((:*:)), type (<:*:>), (<:*:>))
-import Pandora.Paradigm.Algebraic.Functor ((<-*--), extract, point, void)
+import Pandora.Paradigm.Algebraic.Product ((:*:) ((:*:)), type (<:*:>), (<:*:>), attached)
+import Pandora.Paradigm.Algebraic.Functor ((<-*--), (-+), extract, point, void, until)
 import Pandora.Paradigm.Schemes.TT (TT (TT), type (<::>))
 import Pandora.Paradigm.Schemes.T_U (T_U (T_U))
+import Pandora.Paradigm.Schemes.TUT (TUT (TUT))
 import Pandora.Paradigm.Schemes.P_Q_T (P_Q_T (P_Q_T))
 import Pandora.Paradigm.Primary.Auxiliary (Vertical (Up, Down), Horizontal (Left, Right))
 import Pandora.Paradigm.Primary.Functor.Exactly (Exactly (Exactly))
+import Pandora.Paradigm.Primary.Functor.Maybe (Maybe (Just))
 import Pandora.Paradigm.Primary.Transformer.Reverse (Reverse (Reverse))
-import Pandora.Paradigm.Controlflow.Effect.Adaptable (adapt)
-import Pandora.Paradigm.Controlflow.Effect.Transformer ((:>), wrap)
+import Pandora.Paradigm.Controlflow.Effect.Transformer ((:>) (TM), wrap)
 import Pandora.Paradigm.Structure.Ability.Morphable (Occurrence (All))
 import Pandora.Paradigm.Structure.Ability.Substructure (Substructure (Substance, substructure), Segment (Root, Branch, Rest), sub)
 import Pandora.Paradigm.Structure.Ability.Slidable (Slidable (Sliding, slide))
 import Pandora.Paradigm.Structure.Interface.Stack (Stack (Topping, push, pop))
+import Pandora.Paradigm.Structure.Modification.Turnover (Turnover (Turnover))
 import Pandora.Paradigm.Inventory.Some.State (State, change)
 import Pandora.Paradigm.Inventory.Some.Store (Store (Store))
-import Pandora.Paradigm.Inventory.Some.Optics (view, replace, mutate, transwrap)
+import Pandora.Paradigm.Inventory.Some.Optics (view, replace, mutate, transwrap, primary)
 import Pandora.Paradigm.Inventory (zoom, overlook)
 
 type Tape structure = Exactly <:*:> Reverse structure <:*:> structure
@@ -91,3 +93,18 @@ instance (Covariant (->) (->) structure, Stack structure, Bindable (->) (Topping
 			====<< wrap . zoom @(Tape structure e) (sub @Root) . overlook . change . constant
 				====<< lift ====<< wrap <---- zoom @(Tape structure e) <--- sub @Rest
 					<--- zoom <-- sub @Left <-- zoom transwrap pop
+
+-- TODO: generalize this instance over direction, but for this we need a defintion of opposite derection
+instance (Covariant (->) (->) structure, Stack structure, Topping structure ~ Maybe) => Slidable Left (Turnover > Tape structure) where
+	type Sliding Left (Turnover > Tape structure) = Exactly
+	slide :: forall e . State (Turnover < Tape structure < e) :> Exactly >>> ()
+	slide = TM . TUT <-- \(Turnover tape) ->
+		let Just updated = attached <-|- slide @Left -+ until (slide @Right) <~~ tape in
+		Exactly <--- Turnover updated :*: ()
+
+instance (Covariant (->) (->) structure, Stack structure, Topping structure ~ Maybe) => Slidable Right (Turnover > Tape structure) where
+	type Sliding Right (Turnover > Tape structure) = Exactly
+	slide :: forall e . State (Turnover < Tape structure < e) :> Exactly >>> ()
+	slide = TM . TUT <-- \(Turnover tape) ->
+		let Just updated = attached <-|- slide @Right -+ until (slide @Left) <~~ tape in
+		Exactly <--- Turnover updated :*: ()
