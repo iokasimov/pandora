1a582ce951125cbc52119e28aee71975f10f246a
Murat Kasimov <iokasimov.m@gmail.com>
2022-05-28 20:48:57 +0700

    Redefine Tensor morphism

--- Pandora/Pattern/Morphism/Tensor.hs
+++ Pandora/Pattern/Morphism/Tensor.hs
@@ -1,3 +1,6 @@
 module Pandora.Pattern.Morphism.Tensor where
 
-newtype Tensor t p q m l r = Tensor (m (p (t l) (t r)) (t (q l r)))
+-- newtype Tensor t p q m l r = Tensor (m (p (t l) (t r)) (t (q l r))
+
+data Tensor p q m l r where
+	Tensor :: m (p (t l) (t r)) (t (q l r)) -> Tensor p q m (t l) (t r)
